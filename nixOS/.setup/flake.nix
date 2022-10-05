b3d68234dd6d10a49514113cb7a2be73ad172d21
Yeshey <41551785+Yeshey@users.noreply.github.com>
2022-10-05 16:24:56 +0100

    k

--- nixOS/.setup/flake.nix
+++ nixOS/.setup/flake.nix
@@ -3,25 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = inputs @ { self, nixpkgs, home-manager, ...}:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      lib = nixpkgs.lib;
+      #system = "x86_64-linux";
+      #pkgs = import nixpkgs {
+      #  inherit system;
+      #  config.allowUnfree = true;
+      #};
+      #lib = nixpkgs.lib;
+      location = "$HOME/.setup";
       user = "yeshey";
     in {
       nixosConfigurations = {
-        ${user} = lib.nixosSystem {
-          inherit system;
-          modules = [ ./configuration.nix ];
-
-        };
+        import ./hosts {          # if no files is specified, it will always go to default.nix in the directory                                           # Imports ./hosts/default.nix
+          inherit (nixpkgs) lib;
+          inherit inputs nixpkgs home-manager nur user location doom-emacs hyprland;   # Also inherit home-manager so it does not need to be defined here.
+        }
       };
+
+      homeConfigurations = (                                                # Non-NixOS configurations
+        import ./nix {
+          inherit (nixpkgs) lib;
+          inherit inputs nixpkgs home-manager nixgl user;
+        }
+      );
+
     };
 
 }
