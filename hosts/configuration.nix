8c2a559ebe3358b80dc82c54c2ad482c8d1e80c3
Yeshey <41551785+Yeshey@users.noreply.github.com>
2022-10-24 16:36:49 +0100

    preparing for gnome on surface

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -48,24 +48,6 @@
   #  desktopManager.gnome.enable = true;
   #};
 
-  # KDE Plasma
-  services.xserver = {
-    enable = true; # Enable the X11 windowing system.
-    displayManager = {
-      autoLogin.enable = true;
-      autoLogin.user = "yeshey";
-      sddm = {
-        enable = true;
-      };
-      defaultSession = "plasma"; # "none+bspwm" or "plasma"
-    };
-    desktopManager.plasma5 = {
-      enable = true;
-      # supportDDC = true; # doesnt work with nvidia # to support changing brightness for external monitors (https://discourse.nixos.org/t/how-to-enable-ddc-brightness-control-i2c-permissions/20800)
-    };
-    # windowManager.bspwm.enable = true; # but doesn't work
-  };
-
   # Configure keymap in X11
   services.xserver = {
     layout = "pt";
@@ -254,13 +236,6 @@
 
     # tmp
     # texlive.combined.scheme-full # LaTeX
-    
-    # FOR PLASMA DESKTOP
-    scrot # for plasma config saver widget
-    kdialog # for plasma config saver widget
-    ark # Compress and Uncompress files
-    sddm-kcm # for sddm configuration in settings
-    kate # KDEs notepad
 
     # SHELL
     oh-my-zsh
--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -33,6 +33,24 @@
     };
   };
 
+  # KDE Plasma
+  services.xserver = {
+    enable = true; # Enable the X11 windowing system.
+    displayManager = {
+      autoLogin.enable = true;
+      autoLogin.user = "yeshey";
+      sddm = {
+        enable = true;
+      };
+      defaultSession = "plasma"; # "none+bspwm" or "plasma"
+    };
+    desktopManager.plasma5 = {
+      enable = true;
+      # supportDDC = true; # doesnt work with nvidia # to support changing brightness for external monitors (https://discourse.nixos.org/t/how-to-enable-ddc-brightness-control-i2c-permissions/20800)
+    };
+    # windowManager.bspwm.enable = true; # but doesn't work
+  };
+
   environment.systemPackages = with pkgs; [
 
     # Epic_Games_Claimer
@@ -44,6 +62,13 @@
     # Games
     osu-lazer
     lutris
+
+    # FOR PLASMA DESKTOP
+    scrot # for plasma config saver widget
+    kdialog # for plasma config saver widget
+    ark # Compress and Uncompress files
+    sddm-kcm # for sddm configuration in settings
+    kate # KDEs notepad
 
     blender # for blender
   ];
