3fc4c49fd96a86d8d678e3f6a59f254e10781f30
Yeshey <yesheysangpo@hotmail.com>
2022-10-28 21:25:03 +0100

    Surface should work

--- hosts/surface/hardware-configuration.nix
+++ hosts/surface/hardware-configuration.nix
@@ -29,8 +29,6 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
@@ -40,4 +38,5 @@
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+ 
 }
