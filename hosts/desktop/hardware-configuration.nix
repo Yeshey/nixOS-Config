2955ce53b0b4969a04301efa57f982686f471f88
Yeshey <yesheysangpo@hotmail.com>
2022-11-16 01:09:43 +0000

    prismlauncher MC had to rebuild EVERYTHING

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -30,8 +30,6 @@
       options = [ "subvol=swap" ];
     };
 
-  # swapDevices = [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
