f62842006cf18947913878ff43097096ffdbb2a4
Yeshey <yesheysangpo@hotmail.com>
2023-01-23 15:03:01 +0000

    + comment

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -197,9 +197,10 @@
     enable = true;
     # package = pkgs.bluezFull;
   };
-  # https://github.com/NixOS/nixpkgs/issues/63703
-  # https://discourse.nixos.org/t/how-to-override-nixpkg-services-execstart/17699
-  systemd.services.bluetooth.serviceConfig.ExecStart = [  #I guess you don't need this: lib.mkForce
+  # https://github.com/NixOS/nixpkgs/issues/63703 (issue that helped me override it)
+  # https://discourse.nixos.org/t/how-to-override-nixpkg-services-execstart/17699 (general systemd service override)
+  # https://forum.manjaro.org/t/how-to-monitor-battery-level-of-bluetooth-device/117769 (where I found the solution to report connected bluetooth devices battery)
+  systemd.services.bluetooth.serviceConfig.ExecStart = [  # I guess you don't need this: lib.mkForce
     ""
     "${pkgs.bluez}/libexec/bluetooth/bluetoothd -f /etc/bluetooth/main.conf --experimental" 
   ];
