be38cf1adb7d00a3419655f07a8dab0ea22e7d5f
Yeshey <yesheysangpo@hotmail.com>
2023-02-22 20:42:27 +0000

    VSCodium now using the MS VSC extension store
    and python actually works now hopefully

--- hosts/nixFiles/vscode-settings.nix
+++ hosts/nixFiles/vscode-settings.nix
@@ -1,6 +1,5 @@
 {
   "files.autoSave" = "afterDelay"; # basically on
   "java.jdt.ls.java.home" = "/run/current-system/sw/lib/openjdk/"; # Show VSCodium where jdk is
-  "code-runner.runInTerminal" = true;
   "nameLong" = "Visual Studio Code"; # to make pylance work (https://github.com/VSCodium/vscodium/issues/892#issuecomment-957850440)
 }
