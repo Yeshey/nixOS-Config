708f87e842c5596c7a2abf5165ec91c08ffea2ed
Yeshey <yesheysangpo@hotmail.com>
2023-02-24 01:35:33 +0000

    don't think I need python.defaultInterpreter VSC

--- hosts/home.nix
+++ hosts/home.nix
@@ -8,8 +8,10 @@ let
   vscUserSettings = {
     "files.autoSave" = "afterDelay"; # basically on
     "java.jdt.ls.java.home" = "/run/current-system/sw/lib/openjdk/"; # Show VSCodium where jdk is
-    "nameLong" = "Visual Studio Code"; # to make pylance work (https://github.com/VSCodium/vscodium/issues/892#issuecomment-957850440)
-    "python.defaultInterpreterPath" = "${pkgs.python3}";
+    "code-runner.runInTerminal" = true;
+    "code-runner.executorMap" = {
+      "python" = "python3 -u";
+    };
   };
 in
 {
