457012bdb48dbbb73c61836c6fcd3fa743761b5f
Yeshey <yesheysangpo@hotmail.com>
2023-03-23 22:44:48 +0000

    how to add startup apps with home-manager
    added comment for possible code

--- hosts/home.nix
+++ hosts/home.nix
@@ -13,8 +13,28 @@ let
       "python" = "python3 -u";
     };
   };
+  #autostartPrograms = [ pkgs.discord pkgs.slack ];
 in
 {
+    # https://github.com/nix-community/home-manager/issues/3447
+    /*home.file = builtins.listToAttrs (map
+      (pkg:
+        {
+          name = ".config/autostart/" + pkg.pname + ".desktop";
+          value =
+            if pkg ? desktopItem then {
+              # Application has a desktopItem entry. 
+              # Assume that it was made with makeDesktopEntry, which exposes a
+              # text attribute with the contents of the .desktop file
+              text = pkg.desktopItem.text;
+            } else {
+              # Application does *not* have a desktopItem entry. Try to find a
+              # matching .desktop name in /share/apaplications
+              source = (pkg + "/share/applications/" + pkg.pname + ".desktop");
+            };
+        })
+      autostartPrograms);*/
+
     # ====== Making VScodium settings writable ======
     # Allowing VScode to change settings on run time, see last response: https://github.com/nix-community/home-manager/issues/1800
     # VScodium is now free to write to its settings, but they will be overwritten when I run nixos rebuild
@@ -95,6 +115,7 @@ in
         krita
         inkscape
         arduino
+        premid # show youtube videos watching in discord
         # etcher #insecure?
 
         # Browsers
