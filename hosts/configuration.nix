d020252adca9984fc6eb34703db90092bf0e933a
Yeshey <yesheysangpo@hotmail.com>
2023-04-09 20:56:14 +0100

    hash mistake

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -555,7 +555,7 @@
       discord = super.discord.overrideAttrs (
         _: { src = builtins.fetchTarball {
           url = "https://discord.com/api/download?platform=linux&format=tar.gz"; 
-          sha256 = "sha256:04r1yx6aqd4f41q7wfcgs3jfpn40gz7gwajzai1aqz12ny78rs7z";
+          sha256 = "sha256:04r1yx6aqd4f4lq7wfcgs3jfpn40gz7gwajzai1aqz12ny78rs7z";
         };}
       );
     })
