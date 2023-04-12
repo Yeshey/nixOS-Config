9748aeb5c9cabe57fb49b4b363c297678b120748
Yeshey <yesheysangpo@hotmail.com>
2023-04-12 12:47:56 +0100

    VM with plasma

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -557,7 +557,7 @@
       discord = super.discord.overrideAttrs (
         _: { src = builtins.fetchTarball {
           url = "https://discord.com/api/download?platform=linux&format=tar.gz"; 
-          sha256 = "sha256:12yrhlbigpy44rl3icir3jj2p5fqq2ywgbp5v3m1hxxmbawsm6wi";
+          sha256 = "sha256:04r1yx6aqd4f4lq7wfcgs3jfpn40gz7gwajzai1aqz12ny78rs7z";
         };}
       );
     })
