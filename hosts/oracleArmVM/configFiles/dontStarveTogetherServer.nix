f124f00272dfd25e57eb20c941eb11ba62987d60
Yeshey <yesheysangpo@hotmail.com>
2023-06-24 20:49:20 +0100

    trying to make dont starve work on arm server

--- hosts/oracleArmVM/configFiles/dontStarveTogetherServer.nix
+++ hosts/oracleArmVM/configFiles/dontStarveTogetherServer.nix
@@ -4,15 +4,15 @@
   # Dont starve together server:
   virtualisation.oci-containers.containers = {
     dst-server = {
-      image = "jamesits/dst-server:latest";
+      image = "docker.io/jamesits/dst-server:latest";
       volumes = [
-        "${dataStoragePath}/Servers/DoNotStarveTogetherServer:/data:rw"
+        "${dataStoragePath}/Servers/dontstarve/serv-Survival1/DoNotStarveTogetherServer:/data:rw"
       ];
       environment = {
         DST_SERVER_ARCH = "amd64";
         DST_CLUSTER_TOKEN = "pds-g^KU_sJttONn9^Mk79OnW7xsITFY4PnG7ME+WS9Or86mhQ7+/kNzUGb30=";
       };
-      extraOptions = [];
+      extraOptions = [ "--platform=linux/amd64" ];
       ports = [
         "10999-11000:10999-11000/udp"
         "12346-12347:12346-12347/udp"
@@ -20,6 +20,10 @@
       autoStart = true;
     };
   };
+  environment.systemPackages = with pkgs; [
+    qemu
+  ];  
+  #boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   networking.firewall.allowedUDPPorts = [
     10999
     11000
