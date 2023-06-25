f6e5f719ebdde3548fce037dff774d1fd4960108
Yeshey <yesheysangpo@hotmail.com>
2023-06-25 12:32:45 +0100

    this line makes system unbootable

--- hosts/oracleArmVM/configFiles/dontStarveTogetherServer.nix
+++ hosts/oracleArmVM/configFiles/dontStarveTogetherServer.nix
@@ -22,8 +22,15 @@
   };
   environment.systemPackages = with pkgs; [
     qemu
-  ];  
-  #boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  ];
+  # boot.binfmt.emulatedSystems = [ "x86_64-linux" ];  
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ]; # this breaks with this error:
+  /*
+restarting the following units: nix-daemon.service
+open3: exec of /nix/store/j1c20f57gn0y9y9kr61dlgjll2hlhbnp-system-path/bin/busctl --json=short call org.freedesktop.systemd1 /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager ListUnitsByPatterns asas 0 0 failed: Too many levels of symbolic links at /nix/store/82a9ld0177rnnsfklwlf27bvk733vyln-perl-5.36.0/lib/perl5/5.36.0/IPC/Cmd.pm line 1531.
+warning: error(s) occurred while switching to the new configuration
+/nix/store/y0yp1wyhlkgd8vy638pajania07ycmxm-nixos-rebuild/bin/nixos-rebuild: line 384: /nix/store/sdfpzhcksqwsfya89ldci3yhvw6sywg9-coreutils-9.1/bin/rm: Too many levels of symbolic links
+  */
   networking.firewall.allowedUDPPorts = [
     10999
     11000
