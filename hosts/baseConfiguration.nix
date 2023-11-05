300b3f3f945a7a7e6cd28838a54645b407b15b93
Yeshey <yesheysangpo@hotmail.com>
2023-11-05 21:42:23 +0000

    no time to fix this now
    will install github-desktop on flathub

--- hosts/baseConfiguration.nix
+++ hosts/baseConfiguration.nix
@@ -180,7 +180,7 @@
       shellAliases = {
         vim = "nvim";
         # ls = "lsd -l --group-dirs first";
-        update = "sudo nixos-rebuild switch --flake ${location}#${host} --impure"; # old: "sudo nixos-rebuild switch";
+        update = "sudo nixos-rebuild switch --flake ${location}#${host}"; # --impure # old: "sudo nixos-rebuild switch";
         update-re = "sudo nixos-rebuild boot --flake ${location}#${host} --impure && reboot"; # old: "sudo nixos-rebuild switch";
         upgrade = "trap \"cd ${location} && git checkout -- flake.lock\" INT ; sudo nixos-rebuild switch --flake ${location}#${host} --upgrade --update-input nixos-hardware --update-input nixos-nvidia-vgpu --update-input home-manager --update-input nixpkgs --impure || (cd ${location} && git checkout -- flake.lock)"; /*--commit-lock-file*/ #upgrade: upgrade NixOS to the latest version in your chosen channel";
         clean = "echo \"This will clean all generations, and optimise the store\" ; sudo sh -c 'nix-collect-garbage -d ; nix-store --optimise'";
