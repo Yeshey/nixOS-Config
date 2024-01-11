a084b6f560eb123246794e9d1360af1e724e8fdf
Yeshey <yesheysangpo@hotmail.com>
2024-01-11 13:10:58 +0100

    latex in oracleVM

--- hosts/oracleArmVM/default.nix
+++ hosts/oracleArmVM/default.nix
@@ -66,11 +66,28 @@ in
   ];
   */
 
+  /*
+  # SOund redirection not working
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  }; */
+
+  # Remote Desktop with XRDP
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-
   services.xrdp.enable = true;
   services.xrdp.defaultWindowManager = "startplasma-x11";
   networking.firewall.allowedTCPPorts = [ 3389 ];
