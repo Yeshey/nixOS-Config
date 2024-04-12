3797489e77dc5f80a58c1a4fa2c74934d0f4db01
Yeshey <yesheysangpo@hotmail.com>
2024-04-12 17:48:50 +0100

    alacritty

--- modules/home-manager/myHome/gnome/default.nix
+++ modules/home-manager/myHome/gnome/default.nix
@@ -7,25 +7,9 @@ in
 {
   imports = [ 
     ./dconf.nix 
-    #./alacritty.nix # TODO make a option for the terminal emulator, shouldn-t be inside gnome, and should still get the template colours
-    #./kitty.nix
   ];
   options.myHome.gnome = with lib; {
     enable = mkEnableOption "gnome";
-    /*
-    wallpaper = mkOption {
-      type = types.package;
-      default = 
-      builtins.fetchurl {
-        url = "https://images6.alphacoders.com/655/655990.jpg";
-        sha256 = "b09b411a9c7fc7dc5be312ca9e4e4b8ee354358daa792381f207c9f4946d95fe";
-      };
-      /*
-      builtins.fetchurl {
-        url = "https://cdna.artstation.com/p/assets/images/images/018/711/480/large/john-kearney-cityscape-poster-artstation-update.jpg";
-        sha256 = "sha256:1a2krq61502z5zka0a97zll4s8x9dv2qaap5hivpr7fpzl46qp2n";
-      }; 
-    }; */ 
     font = {
       package = mkOption {
         type = types.package;
