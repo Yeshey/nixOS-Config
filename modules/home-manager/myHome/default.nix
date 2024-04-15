f66af8c9e1d6e462f09453bfa1f082517e89f512
Yeshey <yesheysangpo@hotmail.com>
2024-04-15 01:20:00 +0100

    colorSchemeFromPicture

--- modules/home-manager/myHome/default.nix
+++ modules/home-manager/myHome/default.nix
@@ -1,7 +1,14 @@
-{ inputs, config, lib, pkgs, ... }:
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.myHome;
+  nix-colors-lib = inputs.nix-colors.lib.contrib { inherit pkgs; };
 in
 {
   imports = [
@@ -20,20 +27,62 @@ in
     ./alacritty.nix
   ];
   options.myHome = with lib; {
+    # need rec (recursive) to use wallpaper variable in colorScheme
     wallpaper = mkOption {
       type = types.nullOr types.package;
       default = null;
       # default = pkgs.wallpapers.stellarCollisionByKuldarleement;
     };
+    /*
+      wallpaper = mkOption {
+        type = types.package;
+        # default = null;
+        default = pkgs.wallpapers.stellarCollisionByKuldarleement;
+      };
+    */
     colorScheme = mkOption {
-      type = types.attrs;
-      default = inputs.nix-colors.colorSchemes.rose-pine-moon;
-      #type = types.lazyAttrsOf appType;
-      #default = {}; # inputs.nix-colors.colorSchemes.ocean; # by default use all
-      #default = ""; # by default use all # TODO by default, nothing?
+      type = types.nullOr types.attrs;
+      default =
+        if cfg.wallpaper == null then
+          null
+        else
+          nix-colors-lib.colorSchemeFromPicture {
+            path = cfg.wallpaper;
+            variant = "dark"; # TODO expose this option
+          }; # TODO expose option so you can have a wallpaper without it stating the theme
+      #default = nix-colors-lib.colorSchemeFromPicture {
+      #  path = cfg.wallpaper; #  mkIfElse ( wallpaper != null ) pkgs.wallpapers.stellarCollisionByKuldarleement pkgs.wallpapers.nierAutomataWallpaper;
+      #  variant = "dark"; # TODO expose this option
+      #};
+
+      # mkIfElse ( wallpaper != null ) pkgs.wallpapers.stellarCollisionByKuldarleement pkgs.wallpapers.nierAutomataWallpaper;
+      #default = nix-colors-lib.colorSchemeFromPicture {
+      #  path = pkgs.wallpapers.stellarCollisionByKuldarleement;
+      #wallpaper.path; # ./../../../pkgs/wallpapers/StellarCollisionByKuldarLeement.jpg;
+      #  variant = "dark"; # TODO expose this option
+      #};
+      # default = inputs.nix-colors.colorSchemes.rose-pine-moon;
+      #  lib.mkIf ( wallpaper != null )
     };
+
+    #nix-colors-lib.colorSchemeFromPicture {
+    #  path = ./../../../pkgs/wallpapers/StellarCollisionByKuldarLeement.jpg;
+    #  variant = "light";
+    #};
+    #wallpaper;
+    /*
+      mkOption {
+        type = types.attrs;
+        default = inputs.nix-colors.colorSchemes.rose-pine-moon;
+        #type = types.lazyAttrsOf appType;
+        #default = {}; # inputs.nix-colors.colorSchemes.ocean; # by default use all
+        #default = ""; # by default use all # TODO by default, nothing?
+      };
+    */
   };
   config = {
+    #config.colorScheme = inputs.nix-colors.colorSchemes.rose-pine-moon;
+    # cfg.colorScheme = pkgs.wallpapers.stellarCollisionByKuldarleement;
     #colorscheme = cfg.colorScheme;
     #colorScheme = inputs.nix-colors.colorSchemes.${cfg.passthru};
     #colorscheme = inputs.nix-colors.colorSchemes.rose-pine-moon;
--- modules/home-manager/myHome/kitty.nix
+++ modules/home-manager/myHome/kitty.nix
@@ -1,101 +1,112 @@
-{ inputs, config, lib, pkgs, ... }:
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   c = config.myHome.colorScheme.palette;
-  # c = inputs.nix-colors.colorSchemes.ocean.palette;
   cfg = config.myHome.kitty;
   fontName = config.myHome.gnome.font.name;
   fontSize = config.myHome.gnome.font.size;
-in {
+in
+{
   options.myHome.kitty = with lib; {
     enable = mkEnableOption "kitty";
   };
 
   config = lib.mkIf cfg.enable {
     programs.kitty = {
-        enable = true;
-        package = pkgs.kitty;
-        shellIntegration = {
-            enableBashIntegration = true;
-            enableZshIntegration = true;
-        };
-        # theme = "Space Gray Eighties";
-        settings = {
-            # Fonts
-            font_family = "Fira Code";
-            bold_font = "Fira Code Bold";
-            font_size = "14.0";
+      enable = true;
+      package = pkgs.kitty;
+      shellIntegration = {
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+      };
+      # theme = "Space Gray Eighties";
+      settings =
+        {
+          # Fonts
+          font_family = "Fira Code";
+          bold_font = "Fira Code Bold";
+          font_size = "14.0";
 
-            # Cursor customization
-            cursor_blink_interval = 0;
-            cursor_shape = "block";
+          # Cursor customization
+          cursor_blink_interval = 0;
+          cursor_shape = "block";
 
-            # Scrollback
-            scrollback_lines = 2000;
-            wheel_scroll_multiplier = "0.0";
+          # Scrollback
+          scrollback_lines = 2000;
+          wheel_scroll_multiplier = "0.0";
 
-            # Mouse
-            copy_on_select = false;
+          # Mouse
+          copy_on_select = false;
 
-            # Performance tuning
-            repaint_delay = 10;
-            input_delay = 3;
-            sync_to_monitor = true;
+          # Performance tuning
+          repaint_delay = 10;
+          input_delay = 3;
+          sync_to_monitor = true;
 
-            # Terminal bell
-            enable_audio_bell = false;
+          # Terminal bell
+          enable_audio_bell = false;
 
-            # Window layout
-            remember_window_size = true;
-            enabled_layouts = "*";
+          # Window layout
+          remember_window_size = true;
+          enabled_layouts = "*";
 
-            # Color scheme
-            background_opacity = "0.8";
-            foreground = "#${c.base05}"; #"#d3c6aa";
-            background = "#${c.base00}"; #"#272e33";
-            selection_foreground = "#${c.base05}"; #"#d3c6aa";
-            selection_background = "#${c.base02}"; #"#414b50";
+          # Color scheme
+          background_opacity = "0.8";
 
+          wayland_titlebar_color = "background";
+          #linux_display_server = "x11";
 
-            color0 = "#${c.base00}"; #"#272e33";
-            color1 = "#${c.base0E}"; #"#e67e80";
-            color2 = "#${c.base0B}"; #"#83c092";
-            color3 = "#${c.base0A}"; #"#dbbc7f";
-            color4 = "#${c.base08}"; #"#7fbbb3";
-            color5 = "#${c.base09}"; #"#d699b6";
-            color6 = "#${c.base0D}"; #"#a7c080";
-            color7 = "#${c.base06}"; #"#e4e1cd";
-            color8 = "#${c.base02}"; #"#414b50";
-            color9 = "#${c.base0E}"; #"#e67e80";
-            color10 = "#${c.base0D}"; #"#a7c080";
-            color11 = "#${c.base0A}"; #"#dbbc7f";
-            color12 = "#${c.base04}"; #"#9da9a0";
-            color13 = "#${c.base05}"; #"#d3c6aa";
-            color14 = "#${c.base0B}"; #"#83c092";
-            color15 = "#${c.base07}"; #"#fdf6e3";
+          # Keyboard shortcuts
+          "map.kitty_mod" = "ctrl+shift";
+          "map.kitty_mod+e" = "kitten hints";
+          "map.kitty_mod+p+f" = "kitten hints --type path --program -";
+          "map.kitty_mod+p+l" = "kitten hints --type line --program -";
+          "map.kitty_mod+p+w" = "kitten hints --type word --program -";
+          "map.kitty_mod+p+h" = "kitten hints --type hash --program -";
+          "map.kitty_mod+f11" = "toggle_fullscreen";
+          "map.kitty_mod+u" = "kitten unicode_input";
+          "map.kitty_mod+f2" = "edit_config_file";
+          "map.kitty_mod+escape" = "kitty_shell window";
+          "map.kitty_mod+a+m" = "set_background_opacity +0.1";
+          "map.kitty_mod+a+l" = "set_background_opacity -0.1";
+          "map.kitty_mod+a+1" = "set_background_opacity 1";
+          "map.kitty_mod+a+d" = "set_background_opacity default";
+          "map.kitty_mod+delete" = "clear_terminal reset active";
+        }
+        // (
+          if config.myHome.colorScheme != null then
+            {
+              foreground = "#${c.base05}"; # "#d3c6aa";
+              background = "#${c.base00}"; # "#272e33";
+              selection_foreground = "#${c.base05}"; # "#d3c6aa";
+              selection_background = "#${c.base02}"; # "#414b50";
 
-            wayland_titlebar_color = "background";
-            #linux_display_server = "x11";
-
-            # Keyboard shortcuts
-            "map.kitty_mod" = "ctrl+shift";
-            "map.kitty_mod+e" = "kitten hints";
-            "map.kitty_mod+p+f" = "kitten hints --type path --program -";
-            "map.kitty_mod+p+l" = "kitten hints --type line --program -";
-            "map.kitty_mod+p+w" = "kitten hints --type word --program -";
-            "map.kitty_mod+p+h" = "kitten hints --type hash --program -";
-            "map.kitty_mod+f11" = "toggle_fullscreen";
-            "map.kitty_mod+u" = "kitten unicode_input";
-            "map.kitty_mod+f2" = "edit_config_file";
-            "map.kitty_mod+escape" = "kitty_shell window";
-            "map.kitty_mod+a+m" = "set_background_opacity +0.1";
-            "map.kitty_mod+a+l" = "set_background_opacity -0.1";
-            "map.kitty_mod+a+1" = "set_background_opacity 1";
-            "map.kitty_mod+a+d" = "set_background_opacity default";
-            "map.kitty_mod+delete" = "clear_terminal reset active";
-        };
-
+              color0 = "#${c.base00}"; # "#272e33";
+              color1 = "#${c.base0E}"; # "#e67e80";
+              color2 = "#${c.base0B}"; # "#83c092";
+              color3 = "#${c.base0A}"; # "#dbbc7f";
+              color4 = "#${c.base08}"; # "#7fbbb3";
+              color5 = "#${c.base09}"; # "#d699b6";
+              color6 = "#${c.base0D}"; # "#a7c080";
+              color7 = "#${c.base06}"; # "#e4e1cd";
+              color8 = "#${c.base02}"; # "#414b50";
+              color9 = "#${c.base0E}"; # "#e67e80";
+              color10 = "#${c.base0D}"; # "#a7c080";
+              color11 = "#${c.base0A}"; # "#dbbc7f";
+              color12 = "#${c.base04}"; # "#9da9a0";
+              color13 = "#${c.base05}"; # "#d3c6aa";
+              color14 = "#${c.base0B}"; # "#83c092";
+              color15 = "#${c.base07}"; # "#fdf6e3";
+            }
+          else
+            { }
+        );
     };
-
   };
 }
