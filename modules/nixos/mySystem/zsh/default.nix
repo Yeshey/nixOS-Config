e9e5c9b4457126c6de27860063bd7833ef4ac588
Yeshey <yesheysangpo@hotmail.com>
2024-04-17 22:34:44 +0100

    goodbye spaceship, hello starship zsh themes

--- modules/nixos/mySystem/zsh/default.nix
+++ modules/nixos/mySystem/zsh/default.nix
@@ -9,6 +9,39 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    programs.starship = {
+      enable = true;
+      settings = pkgs.lib.importTOML ./starship.toml;
+    };
+    # Need these fonts for starship theme to work
+    fonts.fontconfig.enable = true;
+    fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+      (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+    ];
+    environment = {
+      systemPackages = with pkgs; [
+        nerdfonts
+        /*
+        (
+          nerdfonts.override {
+            fonts = [
+              "FiraCode"
+              "RobotoMono"
+              "SourceCodePro"
+            ];
+          }
+        )*/
+      ];
+    };
 
     programs.zsh = {
       enable = lib.mkDefault true;
@@ -33,28 +66,15 @@ in
         bindkey '^[[Z' reverse-menu-complete
 
         source ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
+        source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+        source ${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use/you-should-use.plugin.zsh
       '';
+# TODO do I need to install them? Figure out
+    #environment.systemPackages = with pkgs; [ 
+    #  zsh-you-should-use
+    #];
 
-      ohMyZsh = {
-        enable = lib.mkDefault true;
-        plugins = [ "git" 
-                    "colored-man-pages" 
-                    "alias-finder" 
-                    "command-not-found" 
-                    "urltools" 
-                    "bgnotify"
-                    "you-should-use"
-                  ];
-        theme = lib.mkDefault "agnoster"; # robbyrussell # agnoster # frisk
-        customPkgs = with pkgs; [
-          zsh-you-should-use
-        ];
-      };
     };
-
-    environment.systemPackages = with pkgs; [ 
-      zsh-you-should-use
-    ];
 
     environment = {
       shells = [ pkgs.zsh ];
