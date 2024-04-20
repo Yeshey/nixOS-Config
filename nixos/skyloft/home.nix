1ce28e70246afde1e832c0e1ad6f335750de227e
Yeshey <yesheysangpo@hotmail.com>
2024-04-20 06:00:00 +0100

    getting ready to deploy to surface and server

--- nixos/skyloft/home.nix
+++ nixos/skyloft/home.nix
@@ -10,15 +10,55 @@ in
   imports = [ ];
 
   myHome = {
-    # gnome.enable = true;
-    tmux.enable = true;
-    zsh.enable = true; # TODO make so it you comment, it should be bash
-    homeapps.enable = false;
-    neovim = {
-      enable = true;
-      enableLSP = true;
+    # All the options
+    user = "yeshey";
+    nonNixos.enable = false;
+    plasma.enable = false;
+    gnome.enable = false;
+    homeApps = {
+      enable = false;
+      cli = {
+        enable = true;
+        general.enable = true;
+        git = {
+          enable = true;
+          personalGit = {
+            enable = true;
+            userName = "Yeshey";
+            userEmail = "yesheysangpo@hotmail.com";
+          };
+        };
+        tmux.enable = true;
+        neovim = {
+          enable = true;
+          enableLSP = true;
+        };
+      };
+      firefox = {
+        enable = false;
+        i2pFirefoxProfile = true;
+      };
+      vscodium.enable = true;
+      discord.enable = false;
+      gaming.enable = false;
+      kitty.enable = false;
+      alacritty.enable = false;
+      libreoffice.enable = false;
+      devops.enable = false;
     };
-    vscodium.enable = true;
+    zsh = {
+      enable = true;
+      starshipTheme = "fredericrous"; # fredericrous # pinage404
+    };
+    direnv.enable = true;
+    #wallpaper = pkgs.wallpapers.johnKearneyCityscapePoster; #johnKearneyCityscapePoster #stellarCollisionByKuldarleement; #nierAutomataWallpaper;
+    #colorScheme = {
+      # theme = colorSchemes.rose-pine-moon;
+    #  setBasedOnWallpaper = { # only takes effect if theme is not set
+    #    enable = true;
+    #    variant = "dark"; # or light
+    #  };
+    #};
   };
 
   home = { # Specific packages # TODO check if you need these
@@ -30,15 +70,6 @@ in
       openvscode-server
       gcc
     ];
-  };
-
-  programs = { # TODO does this do anything?
-    # general terminal shell config for all users
-    zsh = {
-      oh-my-zsh = {
-        theme = lib.mkForce "robbyrussell"; # robbyrussell # agnoster # frisk
-      };
-    };
   };
 
 }
