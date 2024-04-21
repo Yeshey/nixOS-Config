619bebefce0d4d9a57e728d397219f08ecb6ad69
Yeshey <yesheysangpo@hotmail.com>
2024-04-21 03:08:21 +0100

    xfreerdp command comment

--- nixos/skyloft/default.nix
+++ nixos/skyloft/default.nix
@@ -93,6 +93,7 @@
   };
 
   # Remote Desktop with XRDP
+  # xfreerdp /v:143.47.53.175 /u:yeshey /dynamic-resolution /audio-mode:1
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
