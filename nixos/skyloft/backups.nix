63430eeb113ccab2ceffd8c9a2d0ec51048e3803
Yeshey <yesheysangpo@hotmail.com>
2024-04-21 04:07:02 +0200

    borg backups now do to user yeshey

--- nixos/skyloft/backups.nix
+++ nixos/skyloft/backups.nix
@@ -9,15 +9,13 @@ in
     services.borgbackup.jobs = {
       ServerBackups = {
         # Use `sudo borg list -v /mnt/hdd-btrfs/Backups/borgbackup` to check the archives created
+        # Will spit out all the files inside: `sudo borg list /mnt/hdd-btrfs/Backups/borgbackup::<NameOfArchive>`
         # Use `sudo borg info /mnt/hdd-btrfs/Backups/borgbackup::<NameOfArchive>` to see details
         # Use `sudo borg extract /mnt/hdd-btrfs/Backups/borgbackup::<NameOfArchive>` to extract the specified archive to the current directory
         # Use `sudo borg extract /mnt/hdd-btrfs/Backups/borgbackup::nixOS-laptop-mySystemBackup-2023-08-07T00:00:06 /mnt/DataDisk/PersonalFiles/Timeless/Music/AllMusic/` to extract the specified folder in the archive to the current directory
         # Use `sudo borg break-lock /mnt/hdd-btrfs/Backups/borgbackup/` to remove the lock in case you can't access it, make sure nothing is using it
         # Use `sudo systemctl start borgbackup-job-mySystemBackup.service` to make a backup right now
         # Watch size of repo: `watch "sudo du -sh /mnt/hdd-btrfs/Backups/borgbackup/ && echo && sudo du -s /mnt/hdd-btrfs/Backups/borgbackup/"`
-        # TODO dataStoragePath = "/mnt/DataDisk"; and user = "yeshey";
-        # see if the ~ works
-        paths = [ "/var/lib/minetest/.minetest/games/mineclone2" ]; 
         exclude = [ 
             # Largest cache dirs
             ".cache"
@@ -51,24 +49,25 @@ in
             "*/Trash"
             "*/Games"
         ];
-        repo = /home/yeshey/PersonalFiles/Servers/minetest/minetestborgbackup;
         encryption = {
           mode = "none";
         };
-        prune.keep = {
-          within = "1d"; # Keep all archives from the last day
-          daily = 2; # keep the latest backup on each day, up to 7 most recent days with backups (days without backups do not count)
-          weekly = 2; 
-          monthly = 3;
-          yearly = 3;
-        };
-        extraCreateArgs = "--stats";
+        extraCreateArgs = "--stats"; # --umask 0022 sets premissions so syncthing can runconfig
         #encryption = {
         #  mode = "repokey";
         #  passphrase = "secret";
         #};
         compression = "auto,lzma";
+        user = "yeshey";
+        paths = [ "/var/lib/minetest/.minetest/games/mineclone2" ]; 
+        repo = "/home/yeshey/PersonalFiles/Servers/minetest/minetestborgbackup";
+        prune.keep = {
+          within = "1d"; # Keep all archives from the last day
+          weekly = 2; 
+          yearly = 3;
+        };
         startAt = "weekly"; # every 3 hours # "*-*-1/3"; # every 3 days # "hourly"; # weekly # daily # *:0/9 every 9 minutes
+        persistentTimer = true;
       };
     };
 
