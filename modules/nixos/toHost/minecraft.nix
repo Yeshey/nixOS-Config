d219d4a1320809e3d6c400dcc4d3800fbfa0c78f
Yeshey <yesheysangpo@hotmail.com>
2024-05-01 15:33:37 +0200

    path change in comment

--- modules/nixos/toHost/minecraft.nix
+++ modules/nixos/toHost/minecraft.nix
@@ -21,7 +21,7 @@ in
     ];
 
     # run playit-cli launch ./../main_server_config.toml
-    # in here: /home/yeshey/Servers/minecraft/Valhelsia5/server
+    # in here: /home/yeshey/PersonalFiles/Servers/minecraft/Valhelsia5/server
     # The address is quotes-sara.at.ply.gg:18976
     # TODO make a service and add playit.cli to nixpkgs?
   };
