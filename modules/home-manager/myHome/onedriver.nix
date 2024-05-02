bdd8ede254423198290eda932c4d2bd7012b4bfd
Yeshey <yesheysangpo@hotmail.com>
2024-05-02 22:15:21 +0100

    trying to make onedriver work

---
+++ modules/home-manager/myHome/onedriver.nix
@@ -0,0 +1,60 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.myHome.onedriver;
+in
+{
+  options.myHome.onedriver = with lib; {
+    enable = mkEnableOption "onedriver";
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    # https://github.com/NixOS/nixpkgs/issues/291608
+    # if you're here, there has to be a better way already
+
+    # failing with TLS/SSL support not available; install glib-networking
+
+    home.packages = with pkgs; [
+      onedriver
+      glib-networking
+    ];
+
+    # Automount Onedriver
+    systemd.user.services = let
+      wrapperDir = "/run/wrappers/";
+    in {
+
+      "onedriver@mnt-hdd\\x2dbtrfs-Yeshey-OneDriver" = {
+        #shellHook = ''
+        #export GIO_MODULE_DIR=${pkgs.glib-networking}/lib/gio/modules/
+        #'';
+        environment = {
+          GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/";
+        };
+
+        Unit = {
+          Description = "onedriver";
+        };
+
+        Service = {
+          ExecStart = "${pkgs.onedriver}/bin/onedriver /mnt/hdd-btrfs/Yeshey/OneDriver/";
+          ExecStopPost = "${wrapperDir}/bin/fusermount -uz /mnt/hdd-btrfs/Yeshey/OneDriver/";
+          Restart = "on-abnormal";
+          RestartSec = "3";
+          RestartForceExitStatus = "2";
+        };
+
+        Install = {
+          WantedBy = [ "graphical-session.target" ];
+        };
+      };
+    };
+
+  };
+}
