100e5bda6e5308f7484d87e4ac51417d2f8c2b50
Yeshey <yesheysangpo@hotmail.com>
2024-05-23 02:35:28 +0100

    separation of LVM LUKS encryption and cache powers

--- modules/nixos/mySystem/hardware/lvm.nix
+++ modules/nixos/mySystem/hardware/lvm.nix
@@ -11,6 +11,10 @@ in
 {
   options.mySystem.hardware.lvm = with lib; {
     enable = mkEnableOption "lvm";
+
+    cache.enable = mkEnableOption "lvm cache";
+
+    luks.enable = mkEnableOption "lvm luks encryption";
   };
 
   config = lib.mkIf cfg.enable {
@@ -18,38 +22,47 @@ in
     # Tutorial
     # Check everything `sudo lsblk`
     # Check Logical Volumes(LV): df -h /dev/VG/my_lv
- , lvdisplay , and lvscan
     # Check Physical Volumes(PV): pvs , pvdisplay , and pvscan
     # Check Volume Groups(VG): vgs and vgdisplay
     # Tutorial to create a cache on faster drive with LVM: https://gist.github.com/gabrieljcs/805c183753046dcc6131
     # Use Gparted to increase or decrease PVs
 
-    boot.initrd.availableKernelModules = [
-      "usbhid" # not sure if needed
-    ];
+    #boot.initrd.availableKernelModules = [
+    #  "usbhid" # not sure if needed
+    #];
 
     boot.initrd.kernelModules = [
+      # common config
+    ] ++ lib.lists.optionals cfg.cache.enable [
       "dm-cache"
       "dm-cache-smq"
       "dm-cache-mq"
       "dm-cache-cleaner"
+    ] ++ lib.lists.optionals cfg.luks.enable [
+      "aesni_intel"
+      "cryptd"
     ];
 
     boot.kernelModules = [
+      # common config
       "kvm-amd"
-      "dm-cache"
-      "dm-cache-smq"
       "dm-persistent-data"
-      "dm-bio-prison"
       "dm-clone"
-      "dm-crypt"
-      "dm-writecache"
       "dm-mirror"
       "dm-snapshot"
-    ]; # "coretemp" for temp sensors
+    ] ++ lib.lists.optionals cfg.cache.enable [
+      "dm-cache"
+      "dm-cache-smq"
+      "dm-bio-prison"
+      "dm-writecache"
+    ] ++ lib.lists.optionals cfg.luks.enable [
+      "dm-crypt"
+    ];
 
     # for LVM: (https://github.com/NixOS/nixpkgs/issues/15516)
     services.lvm.boot.thin.enable = true;
 
+    #boot.initrd.luks.devices.cryptroot.device = lib.mkIf cfg.luks.enable "/dev/disk/by-uuid/UUID-OF-SDA2";
   };
+
 }
