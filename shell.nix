7227c24d2a137b3dcf6b6be4a1ef9108d68e4da5
Yeshey <yesheysangpo@hotmail.com>
2024-05-24 03:59:51 +0100

    nix-shell working

--- shell.nix
+++ shell.nix
@@ -1,25 +1,15 @@
 # Shell for bootstrapping flake-enabled nix and home-manager
-{
-  pkgs ?
-    let
-      # If pkgs is not defined, instantiate nixpkgs from locked commit
-      lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
-      nixpkgs = fetchTarball {
-        url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
-        sha256 = lock.narHash;
-      };
-      system = builtins.currentSystem;
-      overlays = [ ]; # Explicit blank overlay to avoid interference
-    in
-    import nixpkgs { inherit system overlays; },
-  ...
-}:
-pkgs.mkShell {
-  # Enable experimental features without having to specify the argument
-  NIX_CONFIG = "experimental-features = nix-command flakes";
-  nativeBuildInputs = with pkgs; [
-    nix
-    home-manager
-    git
-  ];
+{pkgs ? import <nixpkgs> {}, ...}: {
+  default = pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
+    nativeBuildInputs = with pkgs; [
+      nix
+      home-manager
+      git
+
+      ssh-to-age
+      gnupg
+      age
+    ];
+  };
 }
