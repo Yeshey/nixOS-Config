89dba11bf7f0df999872d2d051b153308891f03c
Yeshey <yesheysangpo@gmail.com>
2024-06-23 03:42:21 +0100

    Update home.nix

--- nixos/skyloft/home.nix
+++ nixos/skyloft/home.nix
@@ -59,13 +59,12 @@ in
       starshipTheme = "fredericrous"; # fredericrous # pinage404
     };
     direnv.enable = true;
-    /*
     stylix = {
       # https://www.youtube.com/watch?v=ljHkWgBaQWU
       enable = true;
       base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml"; #pop.yaml
       wallpaper = pkgs.wallpapers.johnKearneyCityscapePoster; # johnKearneyCityscapePoster #nierAutomataWallpaper
-    }; */
+    };
     onedriver = {
       enable = true;
       onedriverFolder = "/home/yeshey/OneDriver";
