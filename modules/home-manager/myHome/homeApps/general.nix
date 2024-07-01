a5dce69f0730eaf915275a282eef1e73fe80ba52
Yeshey <yesheysangpo@gmail.com>
2024-07-01 23:47:36 +0100

    disable impermanence, it's a fight for another day

--- modules/home-manager/myHome/homeApps/general.nix
+++ modules/home-manager/myHome/homeApps/general.nix
@@ -19,6 +19,7 @@ in
       directories = [
         ".config/vivaldi/"
       ];
+      allowOther = true;
     };
 
     home = {
--- modules/nixos/mySystem/impermanence.nix
+++ modules/nixos/mySystem/impermanence.nix
@@ -21,45 +21,12 @@ in
     # Using systemd for boot as well.
     # Should have fileSystems."/swap" in a seperate subvolume as well.
     {
-      # https://discourse.nixos.org/t/using-immutable-users-with-impermanence-on-luks/43459
-      # cleans older than 30 days
-/*
-      boot.initrd.systemd.services.clean-old-roots = {
-        requires = ["dev-mapper-cryptroot.device"];
-        after = ["dev-mapper-cryptroot.device"];
-        before = [
-          "sysroot.mount"
-        ];
-        wantedBy = ["initrd.target"];
-        script = ''
-          mkdir /btrfs_tmp
-          mount /dev/disk/by-label/nixos /btrfs_tmp
-          if [[ -e /btrfs_tmp/root ]]; then
-              mkdir -p /btrfs_tmp/old_roots
-              timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-              mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-          fi
-
-          delete_subvolume_recursively() {
-              IFS=$'\n'
-              for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-                  delete_subvolume_recursively "/btrfs_tmp/$i"
-              done
-              btrfs subvolume delete "$1"
-          }
-
-          for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +15); do
-              delete_subvolume_recursively "$i"
-          done
-
-          btrfs subvolume create /btrfs_tmp/root
-          umount /btrfs_tmp
-        '';
-      };*/
       # For /etc/machie-id file already exists https://discourse.nixos.org/t/impermanence-a-file-already-exists-at-etc-machine-id/20267
       #environment.etc.machine-id.source = builtins.toFile "machine-id" "08adbbff76d1468db8c83f09834d622b";
       # I think I can ignore
 
+      # https://discourse.nixos.org/t/using-immutable-users-with-impermanence-on-luks/43459
+      # cleans older than 30 days
       boot.initrd.systemd.services.clean-old-roots = {
         requires = ["dev-mapper-cryptroot.device"];
         after = ["dev-mapper-cryptroot.device"];
