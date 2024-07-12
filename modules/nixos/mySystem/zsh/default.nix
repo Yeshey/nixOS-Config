cfcb0501ea85f20cfd98c5eb494e807126a5923d
Yeshey <yesheysangpo@hotmail.com>
2024-07-13 00:40:19 +0100

    upgrade-remote-off

--- modules/nixos/mySystem/zsh/default.nix
+++ modules/nixos/mySystem/zsh/default.nix
@@ -83,40 +83,39 @@ in
 
             if [ -z "$1" ]; then
                 echo "No IP given! Please provide an IP address."
-                exit 1
-            fi
-
-            REMOTE_IP=$1
-
-            trap "cd '${cfg.falkeLocation}' && git checkout -- flake.lock" INT # if interrupted
-
-            # Ask for password upfront
-            sudo -v
-
-            nix flake update "${cfg.falkeLocation}"
-
-            if sudo nixos-rebuild boot --flake "${cfg.falkeLocation}#${config.mySystem.host}" --build-host root@"''${REMOTE_IP}" --verbose --option eval-cache false; then
-                echo "NixOS upgrade successful."
-
-                # Power off the remote machine
-                ssh -o StrictHostKeyChecking=no root@"''${REMOTE_IP}" "sudo poweroff" && echo "Remote machine powered off."
-
-                # Power off the local machine
-                sudo poweroff && echo "Local machine powered off."
             else
-                echo "Unable to update all flake inputs, trying to update just nixpkgs"
-                cd "${cfg.falkeLocation}" && git checkout -- flake.lock
-                if sudo nixos-rebuild boot --flake "${cfg.falkeLocation}#${config.mySystem.host}" --build-host root@"''${REMOTE_IP}" --verbose --option eval-cache false --update-input nixpkgs; then
-                    echo "NixOS upgrade with nixpkgs update successful."
+                REMOTE_IP=$1
+
+                trap "cd '${cfg.falkeLocation}' && git checkout -- flake.lock" INT # if interrupted
+
+                # Ask for password upfront
+                sudo -v
+
+                nix flake update "${cfg.falkeLocation}"
+
+                if sudo nixos-rebuild boot --flake "${cfg.falkeLocation}#${config.mySystem.host}" --build-host root@"''${REMOTE_IP}" --verbose --option eval-cache false; then
+                    echo "NixOS upgrade successful."
 
                     # Power off the remote machine
-                    ssh  -o StrictHostKeyChecking=noroot@"''${REMOTE_IP}" "sudo poweroff" && echo "Remote machine powered off."
+                    ssh -o StrictHostKeyChecking=no root@"''${REMOTE_IP}" "sudo poweroff" && echo "Remote machine powered off."
 
                     # Power off the local machine
                     sudo poweroff && echo "Local machine powered off."
                 else
-                    echo "NixOS upgrade failed. Rolling back changes to flake.lock"
+                    echo "Unable to update all flake inputs, trying to update just nixpkgs"
                     cd "${cfg.falkeLocation}" && git checkout -- flake.lock
+                    if sudo nixos-rebuild boot --flake "${cfg.falkeLocation}#${config.mySystem.host}" --build-host root@"''${REMOTE_IP}" --verbose --option eval-cache false --update-input nixpkgs; then
+                        echo "NixOS upgrade with nixpkgs update successful."
+
+                        # Power off the remote machine
+                        ssh  -o StrictHostKeyChecking=noroot@"''${REMOTE_IP}" "sudo poweroff" && echo "Remote machine powered off."
+
+                        # Power off the local machine
+                        sudo poweroff && echo "Local machine powered off."
+                    else
+                        echo "NixOS upgrade failed. Rolling back changes to flake.lock"
+                        cd "${cfg.falkeLocation}" && git checkout -- flake.lock
+                    fi
                 fi
             fi
         }
