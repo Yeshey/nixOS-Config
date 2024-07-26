f599026edb8959404eb30894f69f5c6a4bdf4d63
Yeshey <yesheysangpo@hotmail.com>
2024-07-26 16:51:21 +0100

    update-remote-off alias

--- modules/nixos/mySystem/zsh/default.nix
+++ modules/nixos/mySystem/zsh/default.nix
@@ -79,7 +79,7 @@ in
           upgrade-remote-off() {
             export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 
-            echo "This will upgrade the local system with the remote computer with the given IP and then power off both the remote and local machines."
+            echo "This will upgrade the local system with the remote computer with the given IP and then power off both the remote and local machines. \n Run with Example: 'upgrade-remote-off 192.168.1.109'"
 
             if [ -z "$1" ]; then
                 echo "No IP given! Please provide an IP address."
@@ -118,7 +118,33 @@ in
                     fi
                 fi
             fi
-        }
+          }
+          update-remote-off() {
+              export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
+
+              echo "This will update the local system with the remote computer with the given IP and then power off both the remote and local machines. \n Run with Example: 'update-remote-off 192.168.1.109'"
+
+              if [ -z "$1" ]; then
+                  echo "No IP given! Please provide an IP address."
+              else
+                  REMOTE_IP=$1
+
+                  # Ask for password upfront
+                  sudo -v
+
+                  if sudo nixos-rebuild boot --flake "${cfg.falkeLocation}#${config.mySystem.host}" --build-host root@"''${REMOTE_IP}" --verbose --option eval-cache false; then
+                      echo "NixOS update successful."
+
+                      # Power off the remote machine
+                      ssh -o StrictHostKeyChecking=no root@"''${REMOTE_IP}" "sudo poweroff" && echo "Remote machine powered off."
+
+                      # Power off the local machine
+                      poweroff && echo "Local machine powered off."
+                  else
+                      echo "NixOS update failed."
+                  fi
+              fi
+          }
         '')}
 
 
