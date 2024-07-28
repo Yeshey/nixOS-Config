52067c2ec0e4d27456892d6191a6d51e747ea33f
Yeshey <41551785+Yeshey@users.noreply.github.com>
2024-07-28 23:58:11 +0100

    waydroid arm translation with sudo waydroid-script

--- modules/nixos/mySystem/waydroid.nix
+++ modules/nixos/mySystem/waydroid.nix
@@ -15,5 +15,9 @@ in
 
   config = lib.mkIf (config.mySystem.enable && cfg.enable)  {
     virtualisation.waydroid.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      nur.repos.ataraxiasjel.waydroid-script # https://www.reddit.com/r/NixOS/comments/15k2jxc/need_help_with_activating_libhoudini_for_waydroid/
+    ];
   };
 }
