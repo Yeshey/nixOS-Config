ea7725a916003d0aaba3b4196f3ffac05e031015
Yeshey <41551785+Yeshey@users.noreply.github.com>
2024-07-31 13:42:14 +0100

    trying to add nix-on-droid support

---
+++ home-manager/nix-on-droid.nix
@@ -0,0 +1,64 @@
+# TODO what is this
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+
+  myHome = {
+    user = "yeshey";
+    nonNixos.enable = true;
+    gnome.enable = false;
+    devops.enable = false;
+    cli.personalGitEnable = true;
+    tmux.enable = true;
+    zsh.enable = true;
+    neovim = {
+      enable = true;
+      enableLSP = true;
+    };
+  };
+
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    #config = { # TODO remove or find a better way to use overlays?
+    # Disable if you don't want unfree packages
+    #  allowUnfree = true;
+    #};
+  };
+}
