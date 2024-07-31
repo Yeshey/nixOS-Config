354d0a14ea6bb64f99a786547e4a1be57678754c
Yeshey <41551785+Yeshey@users.noreply.github.com>
2024-07-31 14:42:21 +0100

    trying to add nix-on-droid support 7

--- flake.nix
+++ flake.nix
@@ -188,6 +188,11 @@
     };
 
     # Define nixOnDroidConfigurations for Nix-on-Droid
+    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = import nixpkgs { system = "aarch64-linux"; };
+      modules = [ ./home-manager/nix-on-droid.nix ];
+    };
+/*
     nixOnDroidConfigurations = {
       "yeshey@nix-on-droid" = nix-on-droid.lib.nixOnDroidConfiguration {
         pkgs = import inputs.nixpkgs {
@@ -206,7 +211,7 @@
             }
             ./home-manager/nix-on-droid.nix
           ];
-      };
+      };*/
 
     deploy.nodes = {
       hyrulecastle = {
