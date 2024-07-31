a1bb8f6fa5da3569441407be38e5800dfaa947bc
Yeshey <41551785+Yeshey@users.noreply.github.com>
2024-07-31 15:44:45 +0100

    trying to add nix-on-droid support 31

--- modules/home-manager/myHome/default.nix
+++ modules/home-manager/myHome/default.nix
@@ -12,14 +12,25 @@ let
 in
 {
   imports = [
+    ./safe-rm.nix # always active
 
 
+    ./homeApps/default.nix
+    ./hyprland/default.nix
+    ./autoStartApps.nix
+    ./onedriver.nix
+    ./agenix/default.nix
+    ./ssh/default.nix
+    ./stylix.nix
+    ./xdgPersonalFilesOrganization.nix
+    ./impermanence.nix
   ];
   options.myHome = with lib; {
     enable = mkEnableOption "myHome";
     dataStoragePath = mkOption {
       type = types.str;
       description = "Storage drive or path to put everything. needs to be set if not set in mySystem module.";
+      #default = "${osConfig.mySystem.dataStoragePath}";
     };
     user = mkOption {
       type = types.str;
