0f1585fa751d65e074078677de21366f399363e5
YESHEY <yesheysangpo@gmail.com>
2024-12-19 03:05:06 +0100

    server updated

--- nixos/skyloft/default.nix
+++ nixos/skyloft/default.nix
@@ -43,11 +43,11 @@
 
   mySystem = rec {
     enable = true;
-    #plasma = {
-    #  enable = false;
-    #  defaultSession = "plasmax11";
-    #};
-    gnome.enable = true; # TODO activate both plasma and gnome same time, maybe expose display manager
+    plasma = {
+      enable = false;
+      defaultSession = "plasmax11";
+    };
+    gnome.enable = false; # TODO activate both plasma and gnome same time, maybe expose display manager
     ssh.enable = true;
     browser.enable = true;
     cliTools.enable = true;
