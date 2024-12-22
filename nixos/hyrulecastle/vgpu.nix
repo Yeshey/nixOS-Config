57c41ba4b2e5ffcf2a668fd83c5dbf62242657de
Yeshey <41551785+Yeshey@users.noreply.github.com>
2024-12-22 14:57:09 +0000

    corrected samba config

--- nixos/hyrulecastle/vgpu.nix
+++ nixos/hyrulecastle/vgpu.nix
@@ -105,20 +105,20 @@ in
     services.samba = {
       enable = true;
       securityType = "user";
-      extraConfig = ''
-        workgroup = WORKGROUP
-        server string = smbnix
-        netbios name = smbnix
-        security = user 
-        #use sendfile = yes
-        #max protocol = smb2
-        # note: localhost is the ipv6 localhost ::1
-        #hosts allow = 192.168.0. 127.0.0.1 localhost
-        #hosts deny = 0.0.0.0/0
-        guest account = nobody
-        map to guest = bad user
-      '';
-      shares = {
+      settings = {
+        global = {
+          "workgroup" = "WORKGROUP";
+          "server string" = "smbnix";
+          "netbios name" = "smbnix";
+          "security" = "user";
+          #"use sendfile" = "yes";
+          #"max protocol" = "smb2";
+          # note: localhost is the ipv6 localhost ::1
+          #"hosts allow" = "192.168.0. 127.0.0.1 localhost";
+          #"hosts deny" = "0.0.0.0/0";
+          "guest account" = "nobody";
+          "map to guest" = "bad user";
+        };
         hdd-ntfs = {
           path = "/mnt/hdd-ntfs";
           browseable = "yes";
