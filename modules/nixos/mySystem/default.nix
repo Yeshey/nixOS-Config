f6770598ea79d15158d732f30f5548cfcd747614
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-03-18 10:22:47 +0000

    has cuda

--- modules/nixos/mySystem/default.nix
+++ modules/nixos/mySystem/default.nix
@@ -31,7 +31,7 @@ let
     nixcommunitycachixorg = {
       url = "https://nix-community.cachix.org";
       key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
-    };
+    }; # has cuda
     #nixgaming = {
     #  url = "https://nix-gaming.cachix.org";
     #  key = "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=";
