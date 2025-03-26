8b4cc061cbd9bdaa0e6b397b1a55b9148044642a
YESHEY <yesheysangpo@gmail.com>
2025-03-26 01:51:46 +0100

    steam starts to run but crashes with box64

--- nixos/skyloft/box64.nix
+++ nixos/skyloft/box64.nix
@@ -3,80 +3,23 @@
 with lib;
 let
   cfg = config.mySystem.box64;
-  
-  # Use x86_64 packages via cross-compilation
-  pkgsx86 = import inputs.nixpkgs {
-    system = "x86_64-linux";
-    config.allowUnsupportedSystem = true;
-    config.allowUnfree = true;
-    overlays = [
-      (self: super: {
-        # Force 32-bit libs to come from x86_64 package set
-        pkgsi686Linux = super.pkgsi686Linux;
-      })
-    ];
-  };
 
   # Common libraries needed for Steam
-  steamLibs = with pkgsx86; [
-    # 64-bit libraries
-    glibc
-    gcc-unwrapped.lib
-    libgccjit
-    libpng
-    libpulseaudio
-    libjpeg
-    libpng
-    libvorbis
-    stdenv.cc.cc.lib
-    xorg.libX11
-    xorg.libXext
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXfixes
-    xorg.libXcursor
-    xorg.libXi
-    xorg.libXcomposite
-    xorg.libXtst
-    xorg.libSM
-    xorg.libICE
-    libGL
-    libglvnd
-    vulkan-loader
-    freetype
-    openssl
-    curl
-    zlib
-    dbus
-    ncurses
-    SDL2
-    # 32-bit libraries
-    (pkgsi686Linux.glibc)
-    (pkgsi686Linux.gcc-unwrapped.lib)
-    (pkgsi686Linux.libpulseaudio)
-    (pkgsi686Linux.libjpeg)
-    (pkgsi686Linux.xorg.libX11)
-    (pkgsi686Linux.xorg.libXext)
-    (pkgsi686Linux.xorg.libXrandr)
-    (pkgsi686Linux.libGL)
-    (pkgsi686Linux.vulkan-loader)
+  steamLibs = with pkgs; [
+    # libraries
   ];
 
   # FHS environment for Steam
   steamFHS = pkgs.buildFHSUserEnv {
     name = "steam-fhs";
-    targetPkgs = pkgs: (with pkgsx86; [
-      #box64
-      steamPackages.steam
-      # steamPackages.steam-runtime
+    targetPkgs = pkgs: (with pkgs; [
+
     ]) ++ steamLibs;
 
     multiPkgs = pkgs: steamLibs;
 
     extraInstallCommands = ''
-      mkdir -p $out/etc/ld.so.conf.d
-      echo "${pkgsx86.glibc}/lib" > $out/etc/ld.so.conf.d/glibc.conf
-      echo "${pkgsx86.glibc}/lib32" >> $out/etc/ld.so.conf.d/glibc.conf
+      #...
     '';
 
     runScript = "steam";
@@ -98,10 +41,6 @@ in {
     in {
       inherit (x86pkgs) steam steam-run;
     })];
-
-    # programs.steam.enable = true;
-    # hardware.graphics.enable32Bit = lib.mkForce false;
-    # hardware.graphics.enable = lib.mkForce false;
 
     environment.systemPackages = with pkgs; [
       #steam 
@@ -128,6 +67,9 @@ in {
 
 
 /*
+# Using this command to start steam
+/nix/store/x9d49vaqlrkw97p9ichdwrnbh013kq7z-bash-interactive-5.2p37/bin/bash -c "BOX64_LOG=0 mybox64 /nix/store/x9d49vaqlrkw97p9ichdwrnbh013kq7z-bash-interactive-5.2p37/bin/bash /nix/store/2r90fn1idrk09ghra2zg799pff249hmj-steam-unwrapped-1.0.0.81/lib/steam/bin_steam.sh"
+
 You have these bashes rn:
 > file /nix/store/iihnyypprr0ygpdcs5wsawks9mznpd88-bash-interactive-5.2p37/bin/bash                                                                                                 18:15:40
 /nix/store/iihnyypprr0ygpdcs5wsawks9mznpd88-bash-interactive-5.2p37/bin/bash: ELF 64-bit LSB executable, ARM aarch64, version 1 (SYSV), dynamically linked, interpreter /nix/store/8d9rkvllf04pyz790vk6wd4k8mnc5c64-glibc-2.40-36/lib/ld-linux-aarch64.so.1, BuildID[sha1]=5c9d8b11851246b7766f0a7b3042a8988faad435, for GNU/Linux 3.10.0, not stripped
@@ -143,37 +85,23 @@ And try and run steam with:
 
     # Export libraries to current path:
     /*
-export LD_LIBRARY_PATH="$(for lib in \                                                                                                                                            18:13:30
-  glibc \
-  gcc-unwrapped.lib \
-  libgccjit \
-  libpng \
-  libpulseaudio \
-  libjpeg \
-  libvorbis \
-  stdenv.cc.cc.lib \
-  xorg.libX11 \
-  xorg.libXext \
-  xorg.libXrandr \
-  xorg.libXrender \
-  xorg.libXfixes \
-  xorg.libXcursor \
-  xorg.libXi \
-  xorg.libXcomposite \
-  xorg.libXtst \
-  xorg.libSM \
-  xorg.libICE \
-  libGL \
-  libglvnd \
-  vulkan-loader \
-  freetype \
-  openssl \
-  curl \
-  zlib \
-  dbus \
-  ncurses \
-  SDL2 \
-  ; do nix-build '<nixpkgs>' -A ${lib} --no-out-link; done | xargs -I {} echo -n {}/lib: | sed 's/:$//')"
+export LD_LIBRARY_PATH="$(for lib in \                                                                                                                                                            01:39:42
+  glibc glib.out gtk2 gdk-pixbuf pango.out cairo.out \
+  fontconfig libdrm libvdpau expat util-linux \
+  at-spi2-core libnotify gnutls openalSoft udev \
+  xorg.libXinerama xorg.libXdamage xorg.libXScrnSaver \
+  xorg.libxcb libva gcc-unwrapped.lib libgccjit \
+  libpng libpulseaudio libjpeg libvorbis stdenv.cc.cc.lib \
+  xorg.libX11 xorg.libXext xorg.libXrandr xorg.libXrender \
+  xorg.libXfixes xorg.libXcursor xorg.libXi xorg.libXcomposite \
+  xorg.libXtst xorg.libSM xorg.libICE libGL libglvnd \
+  vulkan-loader freetype openssl curl zlib dbus ncurses SDL2 \
+  ; do nix-build '<nixpkgs>' -A ${lib} --no-out-link; done \
+  | xargs -I {} echo -n {}/lib: | sed 's/:$//')\
+:$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/lib/i386-linux-gnu\
+:$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu\
+:$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/lib\
+:$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/usr/lib"
 
   # echo $LD_LIBRARY_PATH
 
@@ -181,27 +109,5 @@ export LD_LIBRARY_PATH="$(for lib in \                                          
 
   binfmt definition ni nixpkgs: https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/system/boot/binfmt.nix
      */
-
-/*
-    # Binfmt configuration for Box64
-    boot.binfmt.registrations.box64 = {
-      interpreter = "${steamFHS}/bin/steam-fhs";
-      magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
-      mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
-    };
-
-    environment.systemPackages = [ steamFHS ];
-
-    # Required environment variables
-    environment.sessionVariables = {
-      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/usr/lib/steam";
-      LD_LIBRARY_PATH = "${pkgsx86.glibc}/lib:${pkgsx86.glibc}/lib32";
-    };
-
-    # Needed for Steam runtime
-    systemd.extraConfig = ''
-      DefaultLimitNOFILE=1048576
-    '';
-    */
   };
 }
