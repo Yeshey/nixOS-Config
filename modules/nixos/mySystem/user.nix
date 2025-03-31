7a15eddd8068f0ce48a27f900e6e568f134a85f5
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-03-31 01:41:31 +0100

    double hop ssh config

--- modules/nixos/mySystem/user.nix
+++ modules/nixos/mySystem/user.nix
@@ -56,6 +56,8 @@ in
         openssh.authorizedKeys.keys = [
           # ssh public key of my_identity key
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDgOfJysYZT/VOwxg/FWCYDnjrSEilzK+YO1JVF5mfkS+eGLWc7IqISNZzPOlNLccIx4vXYr6bAM3wtLAOHajLs4TbnfUe9zfRVO0cGF93eLyOD3VUMVkljgQ4mrt+p2COutvX5j31/JZjAHrp4r/RJiCsWGXib1DGGY52L4g1Ty6pnqY7wErtb56TaHpla/u1BJqHVTTJDg/oZI9BgMObMSRi77QIHZPehmjE04zYz/m2C9fgQuTpHKWU2Ec7zyKp5EuMPWtXbVE0qlZ0J/yiqexu4mT3GRNEIQvo810a1G0uDORxBxP37f3l2PBI0faZk7gCE6baEuh0ejfXhA79TzriWa0yBdevL9pVbMMt9bbolX/CP9lhQX6oaBtWPr2EoXVR1ZyRonya8rqylpYjsPUtAuM35nQSALgsdkXhzuZV2Nw1LLZn0sqaYANmMBKLtDDm3+cOEiXIdFndFI045DvcbfVhdvJeMjrUXGcgFXp+NyAAMa9yY8uMpFKk1qws2eWvEJV1A4gIBJS/bARdcYDwNvH62ASRGNfSkxfWnibLagJgec+a1aUTuEWSqvLJA7lduNC+BZTsWz71h9oBMX6oTqYgyUl1dPOB/+OiVmwfW1tRcAHhxTInEeq7q/GreUUoLk8M33JjwLBF0t4NXj+YqK/zHx+VSZDKoz6ce4w== yeshey@Manjaro-Laptop"
+          # Temporary giveaway key – copy the entire content from ~/.ssh/temp.pub:
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6NKdSPrgQpWWAwoNXwFoOaudUOG5f7alfSiNzMMcF/ Temporary giveaway key"
         ];
         # shell = pkgs.bash;
         #useDefaultShell = false;
