06c7fb34a53a539955a91a73a0bb794866317532
YESHEY <yesheysangpo@gmail.com>
2025-04-06 14:41:25 +0200

    syncthing please

--- modules/nixos/mySystem/syncthing.nix
+++ modules/nixos/mySystem/syncthing.nix
@@ -176,6 +176,7 @@ in
             //*
             //(?i)PhotosAndVideos
             //.git
+            *.ipynb
           "}
 
           # 2025
@@ -184,6 +185,7 @@ in
             //(?i)Photos&PhotosAndVideos
             //.git
             ProjetoIAA
+            *.ipynb
           "}
 
           # 2024
@@ -193,6 +195,7 @@ in
             //.git
             learnWithT*
             AI-Master-Repo-IAA*
+            *.ipynb
           "}
 
           # A70Camera
