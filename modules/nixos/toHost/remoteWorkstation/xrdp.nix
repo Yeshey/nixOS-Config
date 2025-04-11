5d4272b31baa33794d7074798eda0cf6ca67e7de
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-04-11 04:58:54 +0100

    connection methods to oracle in desktopmfiles

--- modules/nixos/toHost/remoteWorkstation/xrdp.nix
+++ modules/nixos/toHost/remoteWorkstation/xrdp.nix
@@ -24,6 +24,31 @@ in
 {
   options.toHost.remoteWorkstation.xrdp = {
     enable = (lib.mkEnableOption "xrdp");
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 3000;
+      example = 3001;
+      description = "port to run vscode-server on";
+    };
+    desktopItem = {
+      enable = lib.mkEnableOption "desktopItem";
+      remote =  {
+        ip = lib.mkOption {
+          type = lib.types.str;
+          default = "143.47.53.175";
+        };
+        user = lib.mkOption {
+          type = lib.types.str;
+          default = "yeshey";
+        };
+      };
+      extraclioptions = lib.mkOption {
+        type = lib.types.str;
+        default = "/dynamic-resolution /kbd:0x0816 /audio-mode:1 /clipboard /network:modem /compression"; # pt keyborad
+        example = "/w:1920 /h:1080 /smart-sizing /kbd:0x0816 /audio-mode:1 /clipboard /network:modem /compression";
+      };
+      
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -32,8 +57,9 @@ in
     #services.displayManager.sddm.enable = true;
     #services.desktopManager.plasma6.enable = true;
     services.xrdp.enable = true;
+    services.xrdp.port = cfg.port;
     services.xrdp.defaultWindowManager = "startplasma-x11";
-    networking.firewall.allowedTCPPorts = [ 3389 ];
+    networking.firewall.allowedTCPPorts = [ cfg.port ];
     services.xrdp.extraConfDirCommands = ''
       substituteInPlace $out/sesman.ini \
         --replace param=.xorgxrdp.%s.log param=/tmp/xorgxrdp.%s.log
@@ -53,5 +79,36 @@ in
       };
     };
 
+  } // lib.mkIf cfg.desktopItem.enable {
+    # makeDesktopItem https://discourse.nixos.org/t/proper-icon-when-using-makedesktopitem/32026
+    environment.systemPackages = with pkgs;     let 
+        govscodeserver = pkgs.writeShellScriptBin "govfreerdpserver" ''
+          ${freerdp}/bin/xfreerdp /v:${cfg.desktopItem.remote.ip} /u:${cfg.desktopItem.remote.user} ${cfg.desktopItem.extraclioptions}
+        '';
+    in [
+      freerdp
+      xdg-utils
+      (makeDesktopItem {
+        name = "FreeRDP Oracle";
+        desktopName = "FreeRDP Oracle";
+        genericName = "FreeRDP Oracle";
+        # Build a command that forwards the port and then opens the browser against the correct URL.
+        exec = "${govscodeserver}/bin/govfreerdpserver";
+        icon = builtins.fetchurl {
+          url = "https://github.com/FreeRDP/FreeRDP/blob/master/client/iOS/Resources/Icon.png?raw=true";
+          sha256 = "sha256:0arbqzzzcmd5m0ysdpydr2mm734vmldjjjbydf1p8njld4kz2klm";
+        };
+
+        categories = [
+          "GTK"
+          "X-WebApps"
+        ];
+        mimeTypes = [
+          "text/html"
+          "text/xml"
+          "application/xhtml_xml"
+        ];
+      })
+    ];
   };
 }
