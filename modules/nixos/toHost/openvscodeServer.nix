010e4a63fa0a8e7bf00c20a44d139b488ac1d71c
YESHEY <yesheysangpo@gmail.com>
2025-04-16 15:43:01 +0200

    openvscode stuff

--- modules/nixos/toHost/openvscodeServer.nix
+++ modules/nixos/toHost/openvscodeServer.nix
@@ -11,6 +11,11 @@ in
 {
   options.toHost.openvscodeServer = {
     enable = (lib.mkEnableOption "openvscodeServer");
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 8443;
+      description = "Remote port number to use for reverse SSH tunneling";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -28,14 +33,18 @@ in
     # (ssh -L 9090:localhost:3000 -t yeshey@143.47.53.175 "sleep 90" &) && xdg-open http://localhost:9090
     services.openvscode-server = {
       enable = true;
+      # package = pkgs.code-server;
       host = "localhost";
-      port = 3000;
+      port = cfg.port;
       user = "yeshey"; # TODO user variable?
       extensionsDir = "/home/yeshey/.vscode-oss/extensions"; # TODO user variable?
       withoutConnectionToken = true; # So you don't need to grab the token that it generates here
     };
 
     networking.firewall.allowedTCPPorts = [
+      cfg.port
+
+      # these are needed for remote-ssh, idk if I even need them here
       80
       443
     ];
