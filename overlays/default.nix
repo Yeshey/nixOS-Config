9e39710faff5648ec403cd56b37c62b927743e7a
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-04-23 00:29:38 +0100

    tmp see if it works

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,6 +19,22 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+    # tmp, to fix build error?
+    python311Packages = prev.python311Packages // {
+      django = prev.python311Packages.django.overrideAttrs (_: {
+        doCheck = false;
+      });
+    };
+    python312Packages = prev.python311Packages // {
+      django = prev.python311Packages.django.overrideAttrs (_: {
+        doCheck = false;
+      });
+    };
+    python313Packages = prev.python311Packages // {
+      django = prev.python311Packages.django.overrideAttrs (_: {
+        doCheck = false;
+      });
+    };
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
