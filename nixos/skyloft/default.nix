455a6ff7a909020d3af9e24eead506b9c606d429
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-04-25 14:53:58 +0100

    syncthing n stuff

--- nixos/skyloft/default.nix
+++ nixos/skyloft/default.nix
@@ -8,7 +8,7 @@
 }:
 
 # To recover in case of loosing access through SSH do the following:
-# Go to Oracle Cloud > Dashboard > Console Connection > Launch Cloud Shell Connection (You might have to delete the current connection and create a new one in "Create local connection" with you public key)
+# Go to Oracle Cloud > Your VM Instance > Resources (below) > Console Connection > Launch Cloud Shell Connection (You might have to delete the current connection and create a new one in "Create local connection" with you public key)
 # Reboot the machine, and during boot select in the console a different generation
 
 {
@@ -42,85 +42,85 @@
 
   mySystem = rec {
     enable = true;
-    plasma = {
-      enable = false;
-      defaultSession = "plasmax11";
-    };
-    gnome.enable = false; # TODO activate both plasma and gnome same time, maybe expose display manager
-    ssh.enable = true;
-    browser.enable = true;
-    cliTools.enable = true;
-    zsh.enable = true;
-    gaming.enable = false;
-    vmHost = true;
-    dockerHost = true;
     host = "skyloft";
     user = "yeshey"; # TODO make this into an option where you can do user."yeshey".home-manager.enable ) true etc.
-    home-manager = {
-      enable = true;
-      home = ./home.nix;
-    };
-    hardware = {
-      enable = true;
-      bluetooth.enable = false;
-      printers.enable = false;
-      sound.enable = true;
-      thermald = {
-        enable = false;
-      };
-      #nvidia.enable = false;
-    };
-    autoUpgrades = {
-      enable = true;
-      location = "github:Yeshey/nixOS-Config"; # "github:Yeshey/nixOS-Config"
-      host = "skyloft";
-      dates = "weekly";
-    };
-    flatpaks.enable = false;
-    i2p.enable = false;
-    syncthing = {
-      enable = true;
-      dataStoragePath = "/home/${user}";
-    };
-    androidDevelopment.enable = false;
-    agenix = {
-      enable = true;
-      sshKeys.enable = true;
-    };
+    # home-manager = {
+    #    enable = false;
+    #    home = ./home.nix;
+    #  };
+    # plasma = {
+    #   enable = false;
+    #   defaultSession = "plasmax11";
+    # };
+    # gnome.enable = false; # TODO activate both plasma and gnome same time, maybe expose display manager
+    # ssh.enable = true;
+    # browser.enable = true;
+    # cliTools.enable = true;
+    # zsh.enable = true;
+    # gaming.enable = false;
+    # vmHost = true;
+    # dockerHost = true;
+    # hardware = {
+    #   enable = true;
+    #   bluetooth.enable = false;
+    #   printers.enable = false;
+    #   sound.enable = true;
+    #   thermald = {
+    #     enable = false;
+    #   };
+    #   #nvidia.enable = false;
+    # };
+    # autoUpgrades = {
+    #   enable = true;
+    #   location = "github:Yeshey/nixOS-Config"; # "github:Yeshey/nixOS-Config"
+    #   host = "skyloft";
+    #   dates = "weekly";
+    # };
+    # flatpaks.enable = false;
+    # i2p.enable = false;
+    # syncthing = {
+    #   enable = true;
+    #   dataStoragePath = "/home/${user}";
+    # };
+    # androidDevelopment.enable = false;
+    # agenix = {
+    #   enable = true;
+    #   sshKeys.enable = true;
+    # };
 
-    borgFolderBackups.jobs = {
-      anarchyMineclone2Server = {
-        repo = "/home/yeshey/PersonalFiles/Servers/luanti/anarchyMineclone2Borgbackup";
-        paths = [ "/var/lib/luanti-anarchyMineclone2/world" ];
-        user = "yeshey";  # This can be omitted if using mySystem.user default
-      };
-      anarchyMinecloniaServer = {
-        repo = "/home/yeshey/PersonalFiles/Servers/luanti/anarchyMinecloniaBorgbackup";
-        paths = [ "/var/lib/luanti-anarchyMineclonia/world" ];
-        user = "yeshey";  # This can be omitted if using mySystem.user default
-      };
-      pixelmonMinecraftServer = {
-        repo = "/home/yeshey/PersonalFiles/Servers/minecraft/pixelmonMinecraftBorgbackup";
-        paths = [ "/srv/minecraft/pixelmon/world" ];
-        user = "yeshey";  # This can be omitted if using mySystem.user default
-      };
-    };
+    # borgFolderBackups.jobs = {
+    #   anarchyMineclone2Server = {
+    #     repo = "/home/yeshey/PersonalFiles/Servers/luanti/anarchyMineclone2Borgbackup";
+    #     paths = [ "/var/lib/luanti-anarchyMineclone2/world" ];
+    #     user = "yeshey";  # This can be omitted if using mySystem.user default
+    #   };
+    #   anarchyMinecloniaServer = {
+    #     repo = "/home/yeshey/PersonalFiles/Servers/luanti/anarchyMinecloniaBorgbackup";
+    #     paths = [ "/var/lib/luanti-anarchyMineclonia/world" ];
+    #     user = "yeshey";  # This can be omitted if using mySystem.user default
+    #   };
+    #   pixelmonMinecraftServer = {
+    #     repo = "/home/yeshey/PersonalFiles/Servers/minecraft/pixelmonMinecraftBorgbackup";
+    #     paths = [ "/srv/minecraft/pixelmon/world" ];
+    #     user = "yeshey";  # This can be omitted if using mySystem.user default
+    #   };
+    # };
   };
 
   box64-binfmt.enable = false;
 
   environment.systemPackages = [ 
-    pkgs.file
-    pkgs.x86.steamcmd
-    pkgs.x86.katawa-shoujo
-    pkgs.x86.cmatrix
-    pkgs.mangohud
-    pkgs.x86.xonotic
-    pkgs.x86.heroic
-    pkgs.x86.superTuxKart
-    pkgs.x86.glmark2
-    pkgs.x86.vulkan-tools
-    pkgs.x86.glxinfo 
+    # pkgs.file
+    # pkgs.x86.steamcmd
+    # pkgs.x86.katawa-shoujo
+    # pkgs.x86.cmatrix
+    # pkgs.mangohud
+    # pkgs.x86.xonotic
+    # pkgs.x86.heroic
+    # pkgs.x86.superTuxKart
+    # pkgs.x86.glmark2
+    # pkgs.x86.vulkan-tools
+    # pkgs.x86.glxinfo 
   ];
 
     nix = {
@@ -142,30 +142,30 @@
     };
 
   toHost = {
-    remoteWorkstation = {
-      sunshine.enable = true;
-      xrdp.enable = true;
-    };
-    dontStarveTogetherServer = {
-      enable = false;
-      path = "/home/yeshey/PersonalFiles/Servers/dontstarvetogether/SurvivalServerMadeiraSummer2/DoNotStarveTogetherServer";
-    };
-    nextcloud.enable = true;
-    minecraft.enable = true;
-    openvscodeServer.enable = true;
-    ngixServer.enable = false;
-    luanti.enable = true;
-    kubo.enable = true;
-    mindustry-server.enable = false;
-    searx.enable = true;
-    ollama = {
-      enable = true; 
-      # acceleration = "cuda"; #or 'rocm' # this issue https://github.com/NixOS/nixpkgs/issues/321920
-    };
-    overleaf.enable = true;
+    # remoteWorkstation = {
+    #   sunshine.enable = true;
+    #   xrdp.enable = true;
+    # };
+    # dontStarveTogetherServer = {
+    #   enable = false;
+    #   path = "/home/yeshey/PersonalFiles/Servers/dontstarvetogether/SurvivalServerMadeiraSummer2/DoNotStarveTogetherServer";
+    # };
+    # nextcloud.enable = true;
+    # minecraft.enable = true;
+    # openvscodeServer.enable = true;
+    # ngixServer.enable = false;
+    # luanti.enable = true;
+    # kubo.enable = true;
+    # mindustry-server.enable = false;
+    # searx.enable = true;
+    # ollama = {
+    #   enable = true; 
+    #   # acceleration = "cuda"; #or 'rocm' # this issue https://github.com/NixOS/nixpkgs/issues/321920
+    # };
+    # overleaf.enable = true;
   };
 
-  boot.binfmt.emulatedSystems = ["i686-linux" "x86_64-linux" "i386-linux" "i486-linux" "i586-linux" "i686-linux"];
+  # boot.binfmt.emulatedSystems = ["i686-linux" "x86_64-linux" "i386-linux" "i486-linux" "i586-linux" "i686-linux"];
 
   time.timeZone = "Europe/Madrid";
 
