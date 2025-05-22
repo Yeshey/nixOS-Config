3dc55934e73b723d403c94be2876e6aba3cff630
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-05-22 02:22:18 +0100

    only on hyrule castle

--- modules/nixos/mySystem/hardware/sound.nix
+++ modules/nixos/mySystem/hardware/sound.nix
@@ -7,33 +7,6 @@
 
 let
   cfg = config.mySystem.hardware.sound;
-
-  hdaJackRetaskFwContent = ''
-    [codec]
-    0x10ec0257 0x17aa3810 0
-
-    [pincfg]
-    0x12 0x90a60120
-    0x13 0x40000000
-    0x14 0x90170110
-    0x18 0x411111f0
-    0x19 0x90a60160
-    0x1a 0x411111f0
-    0x1b 0x411111f0
-    0x1d 0x40661b45
-    0x1e 0x411111f0
-    0x21 0x0421101f
-  '';
-
-  # Create a derivation that produces a directory containing the firmware file
-  hdaJackRetaskFwPkg = pkgs.runCommand "hda-jack-retask-custom-fw" {
-    # buildInputs can be empty if no tools are needed beyond shell builtins
-  } ''
-    # Create the standard directory structure for firmware
-    mkdir -p $out/lib/firmware 
-    # Write the content to the firmware file within that structure
-    echo "${hdaJackRetaskFwContent}" > $out/lib/firmware/hda-jack-retask.fw
-  '';
 in
 {
   options.mySystem.hardware.sound = {
@@ -65,16 +38,6 @@ in
       # no need to redefine it in your config for now)
       #media-session.enable = true;
     };
-
-    # Enable using internal Mic While headphones connected in jack
-    # found out by launching `hdajackretask`, going to Raltek ALC257, set Black Mic Override to "Internal mic" 
-    # Make the firmware file available to the kernel
-    hardware.firmware = [ hdaJackRetaskFwPkg ];
-
-    # Explicitly tell the snd-hda-intel kernel module to load this patch.
-    boot.extraModprobeConfig = ''
-      options snd-hda-intel patch=hda-jack-retask.fw
-    '';
 
   };
 }
--- nixos/hyrulecastle/default.nix
+++ nixos/hyrulecastle/default.nix
@@ -9,6 +9,33 @@
 
 let
   # user = "yeshey";
+
+  hdaJackRetaskFwContent = ''
+    [codec]
+    0x10ec0257 0x17aa3810 0
+
+    [pincfg]
+    0x12 0x90a60120
+    0x13 0x40000000
+    0x14 0x90170110
+    0x18 0x411111f0
+    0x19 0x90a60160
+    0x1a 0x411111f0
+    0x1b 0x411111f0
+    0x1d 0x40661b45
+    0x1e 0x411111f0
+    0x21 0x0421101f
+  '';
+
+  # Create a derivation that produces a directory containing the firmware file
+  hdaJackRetaskFwPkg = pkgs.runCommand "hda-jack-retask-custom-fw" {
+    # buildInputs can be empty if no tools are needed beyond shell builtins
+  } ''
+    # Create the standard directory structure for firmware
+    mkdir -p $out/lib/firmware 
+    # Write the content to the firmware file within that structure
+    echo "${hdaJackRetaskFwContent}" > $out/lib/firmware/hda-jack-retask.fw
+  '';
 in
 {
   imports = [
@@ -342,6 +369,16 @@ in
       '';
     };
   };
+
+  # Enable using internal Mic While headphones connected in jack
+  # found out by launching `hdajackretask`, going to Raltek ALC257, set Black Mic Override to "Internal mic" 
+  # Make the firmware file available to the kernel
+  hardware.firmware = [ hdaJackRetaskFwPkg ];
+
+  # Explicitly tell the snd-hda-intel kernel module to load this patch.
+  boot.extraModprobeConfig = ''
+    options snd-hda-intel patch=hda-jack-retask.fw
+  '';
 
   environment.systemPackages = with pkgs; [ 
     #jetbrains.idea-community-bin
