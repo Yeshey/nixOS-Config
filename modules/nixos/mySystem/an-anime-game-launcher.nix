3ed46f9557f12cc216c4e224064cc66c61f05e34
Yeshey <yesheysangpo@hotmail.com>
2025-07-24 15:08:23 +0100

    set up anime game

--- modules/nixos/mySystem/an-anime-game-launcher.nix
+++ modules/nixos/mySystem/an-anime-game-launcher.nix
@@ -19,8 +19,8 @@ in
 
   config = { 
     # nix.settings = inputs.aagl.nixConfig; # Set up Cachix
-    programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
-    programs.anime-games-launcher.enable = true;
+    programs.anime-game-launcher.enable = true; # Genshin Impact Adds launcher and /etc/hosts rules
+    # programs.anime-games-launcher.enable = true; # all others, but not working?
     #programs.honkers-railway-launcher.enable = true;
     #programs.honkers-launcher.enable = true;
     #programs.wavey-launcher.enable = true;
