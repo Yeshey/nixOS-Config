f9c13b1a863d5feab38eab736f433e778b6ee7c3
Yeshey <yesheysangpo@hotmail.com>
2025-07-26 16:39:12 +0100

    trying a relative path

--- nixos/nix-on-droid/sshd.nix
+++ nixos/nix-on-droid/sshd.nix
@@ -1,4 +1,3 @@
-{ pkgs, ... }:
 {
   # connect with ssh nix-on-droid@192.168.1.254 -p 8022
   # (will not be able to exit app, will have to kill it in android)
@@ -6,7 +5,7 @@
     enable = true;
     ports = [ 8022 ];
     authorizedKeysFiles = [
-      (pkgs.writeText "id_ed25519.pub" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGlaGOK+qN0/Fk0d2bVdRTNncfQwxaEofoOnKgwK95s")
+      ./../../id_ed_mykey.pub
     ];
   };
 }
