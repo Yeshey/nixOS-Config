90e7c35f3e6965181f2b67e08f7d163a699cb38d
Yeshey <yesheysangpo@hotmail.com>
2025-08-05 21:18:42 +0100

    mannn

--- modules/nixos/mySystem/impermanence.nix
+++ modules/nixos/mySystem/impermanence.nix
@@ -105,7 +105,7 @@ in
           "/var/lib/nixos"
           "/var/lib/systemd/coredump"
           "/etc/NetworkManager/system-connections"
-          "/etc/ssh" # keepfor agenix ye
+          { directory = "/etc/ssh"; mode = "0755"; }
           { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
         ];
         files = [
