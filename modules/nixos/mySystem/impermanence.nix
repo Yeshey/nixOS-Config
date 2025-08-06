04617ef641e46a7d9b2325d886e5492c1586dc2c
Yeshey <yesheysangpo@hotmail.com>
2025-08-06 23:17:31 +0200

    lets see if this quells the overleaf docker build monster

--- modules/nixos/mySystem/impermanence.nix
+++ modules/nixos/mySystem/impermanence.nix
@@ -82,7 +82,7 @@ in
             btrfs subvolume delete "$1"
         }
 
-        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +1); do
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +2); do
             delete_subvolume_recursively "$i"
         done
 
