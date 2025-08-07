69c73b889b54dfb7dcb2d0934da0892349695ab9
Yeshey <yesheysangpo@hotmail.com>
2025-08-07 19:32:11 +0100

    Revert "ohhhhh okay"
    This reverts commit 39b37dbfbccc6ccb7cac451e9e56a9fb6cdbd1b3.

--- modules/nixos/toHost/wireguardServer.nix
+++ modules/nixos/toHost/wireguardServer.nix
@@ -1,5 +1,3 @@
-# check pub key cat /etc/wireguard/server.pub
-# helpped by deepseek
 {
   config,
   lib,
@@ -10,63 +8,69 @@
 
 let
   cfg = config.toHost.wireguardServer;
-  
-  # Automatically generate and persist keys
-  serverPrivateKeyFile = "/etc/wireguard/server.key";
-  generateKeys = ''
-    mkdir -p /etc/wireguard
-    if [ ! -f ${serverPrivateKeyFile} ]; then
-      umask 077
-      ${pkgs.wireguard-tools}/bin/wg genkey > ${serverPrivateKeyFile}
-    fi
-  '';
 in
 {
   options.toHost.wireguardServer = with lib; {
     enable = mkEnableOption "wireguardServer";
   };
 
-  config = lib.mkIf cfg.enable {
-    # Key generation service
-    system.activationScripts.wireguardKeys = {
-      text = generateKeys;
-      deps = [];
+  # always active lib.mkIf (config.toHost.enable && cfg.enable) 
+  config = lib.mkIf (cfg.enable) { 
+
+    # enable NAT
+    #networking.nat.enable = true;
+    #networking.nat.externalInterface = "eth0";
+    #networking.nat.internalInterfaces = [ "wg0" ];
+    networking.firewall = {
+      allowedUDPPorts = [ 51820 ];
     };
 
-    networking.firewall.allowedUDPPorts = [ 51820 ];
+    networking.wireguard.enable = true;
+    networking.wireguard.interfaces = {
+      # "wg0" is the network interface name. You can name the interface arbitrarily.
+      wg0 = {
+        # Determines the IP address and subnet of the server's end of the tunnel interface.
+        ips = [ "10.100.0.1/24" ];
 
-    networking.wireguard.interfaces.wg0 = {
-      ips = [ "10.100.0.1/24" ];
-      listenPort = 51820;
-      
-      # Use generated key
-      privateKeyFile = serverPrivateKeyFile;
-      
-      # Automatically extract public key
-      postSetup = let
-        publicKeyScript = pkgs.writeScript "wg-publickey" ''
-          ${pkgs.wireguard-tools}/bin/wg pubkey < ${serverPrivateKeyFile}
+        # The port that WireGuard listens to. Must be accessible by the client.
+        listenPort = 51820;
+
+        # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+        # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+        postSetup = ''
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
         '';
-      in ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-        echo "Server Public Key: $(${publicKeyScript})"
-      '';
 
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-      '';
+        # This undoes the above command
+        postShutdown = ''
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        '';
 
-      peers = [
-        { 
-          name = "Yeshey";
-          publicKey = "8DxCltLVO/wx8/XfXK8ZjXe46WRAQZMAfM59Ijnfu34="; # "{client public key}";
-          allowedIPs = [ "10.100.0.2/32" ];
-        }
-      ];
+        generatePrivateKeyFile = true;
+        privateKeyFile = "ls server.key";
+
+        peers = [
+          # List of allowed peers.
+          { 
+            # Feel free to give a meaningful name
+            name = "Yeshey";
+            # Public key of the peer (not a file path).
+            publicKey = "{client public key}";
+            # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+            allowedIPs = [ "10.100.0.2/32" ];
+          }
+          # { 
+            # ...
+          # }
+        ];
+      };
     };
 
     environment.persistence."/persistent" = {
-      directories = [ "/etc/wireguard" ];
+      directories = [
+        "/etc/wireguard/"
+      ];
     };
+
   };
 }
