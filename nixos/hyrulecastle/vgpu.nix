57a24fec7510d67b351fe832a9937dbc1b03aa69
Yeshey <yesheysangpo@hotmail.com>
2025-08-10 05:06:56 +0100

    specializations compleatly fucking broken n flakes

--- nixos/hyrulecastle/vgpu.nix
+++ nixos/hyrulecastle/vgpu.nix
@@ -1,94 +1,16 @@
 # thanks to https://github.com/MakiseKurisu/nixos-config/blob/main/modules/nvidia-vgpu.nix
 {
-  inputs,
+  vgpu4nixos,
+  fastapi-dls-nixos,
   config,
   pkgs,
   lib,
   ...
 }:
 
-let
-  cfg = config.mySystemHyruleCastle.vgpu; # TODO make it a specialization
-in
 {
-  imports = [
-    #inputs.nixos-nvidia-vgpu.nixosModules.nvidia-vgpu
-    inputs.vgpu4nixos.nixosModules.host
-    inputs.fastapi-dls-nixos.nixosModules.default
-  ];
+
   
-  options.mySystemHyruleCastle.vgpu = {
-    enable = lib.mkEnableOption "NvidiaVgpuSharing";
-  };
+  environment.etc.specialisation.text = "vgpu";
 
-  config = lib.mkIf cfg.enable {
-
-    boot = {
-      kernelPackages = pkgs.linuxPackages_6_6;
-    };
-
-    # https://docs.nvidia.com/vgpu/latest/pdf/grid-vgpu-user-guide.pdf
-    hardware = {
-      nvidia = {
-        package = config.boot.kernelPackages.nvidiaPackages.vgpu_17_3;
-        open = lib.mkForce false;
-        vgpu = {
-          patcher = {
-            enable = true;
-            options.doNotForceGPLLicense = true;
-            # runtimeOptions.enable = true;
-            copyVGPUProfiles = {
-              "1E93:0000" = "1E30:12BA"; # GeForce RTX 2080 SUPER Mobile / Max-Q
-              "1E07:0000" = "1E30:12BA"; # GeForce RTX 2080 Ti Rev. A
-            };
-            profileOverrides = {
-              # GRID RTX6000-1Q
-              "256" = {
-                vramAllocation = 1024;
-                heads = 4;
-                enableCuda = true;
-                display = {
-                  width = 7680;
-                  height = 4320;
-                };
-                framerateLimit = 0;
-              };
-              # GRID RTX6000-6Q
-              "260" = {
-                vramAllocation = 6144;
-                heads = 4;
-                enableCuda = true;
-                display = {
-                  width = 7680;
-                  height = 4320;
-                };
-                framerateLimit = 0;
-              };
-              # GRID RTX6000-24Q
-              "263" = {
-                vramAllocation = 16384;
-                heads = 4;
-                enableCuda = true;
-                display = {
-                  width = 7680;
-                  height = 4320;
-                };
-                framerateLimit = 0;
-              };
-            };
-          };
-          driverSource = {
-            name = "NVIDIA-Linux-x86_64-550.90.05-vgpu-kvm.run";
-            # url get from https://drive.google.com/file/d/1FOD_q0ZA04i9IqaoFEB8I7wcVkPBkkqT/view?usp=sharing
-            sha256 = "sha256-vBsxP1/SlXLQEXx70j/g8Vg/d6rGLaTyxsQQ19+1yp0=";
-          };
-        };
-      };
-    };
-
-    programs.mdevctl = {
-      enable = true;
-    };
-
-  };
 }
