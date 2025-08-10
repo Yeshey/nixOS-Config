54715faec24a68afaf762ce8f505ef5377451f4a
Yeshey <yesheysangpo@hotmail.com>
2025-08-10 15:29:36 +0100

    ya

--- modules/nixos/mySystem/virt.nix
+++ modules/nixos/mySystem/virt.nix
@@ -22,8 +22,8 @@
       users.users.${config.mySystem.user}.extraGroups = [ "libvirtd" ];
       virtualisation.libvirtd.enable = true;
       virtualisation.spiceUSBRedirection.enable = true; # to enable USB rederection in virt-manager (https://github.com/NixOS/nixpkgs/issues/106594)
+      programs.virt-manager.enable = true;
       environment.systemPackages = with pkgs; [
-        virt-manager # virtual machines
         virt-viewer # needed to choose share folders with windows VM (guide and video: https://www.guyrutenberg.com/2018/10/25/sharing-a-folder-a-windows-guest-under-virt-manager/ and https://www.youtube.com/watch?v=Ow3gVbkWj-c)
         spice-gtk # for virtual machines (to connect usbs and everything else)
       ];
