ae20203db412c6a90d1f2ddd9902ad9ac627305f
Yeshey <yesheysangpo@hotmail.com>
2025-08-14 00:05:54 +0100

    capiche?

--- nixos/hyrulecastle/vgpu/nvidia-vgpu-autostart.nix
+++ nixos/hyrulecastle/vgpu/nvidia-vgpu-autostart.nix
@@ -154,6 +154,10 @@
     };
   };
   
+  environment.etc."tmpfiles.d/10-looking-glass.conf".text = ''
+    # Type Path Mode UID GID Age Argument
+  '';
+
   # Link the fallback timer to the fallback service
   systemd.services.create-vgpu-mdevs-fallback.wantedBy = 
     [ "create-vgpu-mdevs-fallback.timer" ];
