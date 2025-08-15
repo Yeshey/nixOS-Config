0f14550d3d94d4f61d28b1d148ce144d83a10c0e
Yeshey <yesheysangpo@hotmail.com>
2025-08-15 16:08:06 +0100

    works in windows guest not in linux guest

--- nixos/hyrulecastle/vgpu/fastapi-dls.nix
+++ nixos/hyrulecastle/vgpu/fastapi-dls.nix
@@ -10,5 +10,12 @@
   services.fastapi-dls = {
     enable = true;
     timezone = "Europe/Lisbon";
+    listen.ip = "192.168.1.169";
   };
+
+  # have to use v1 of fastapi dls for it to work with 17_3
+
+  networking.firewall.allowedTCPPorts = [
+    443
+  ];
 }
--- nixos/hyrulecastle/vgpu/vgpu.nix
+++ nixos/hyrulecastle/vgpu/vgpu.nix
@@ -49,41 +49,41 @@ in
               "1E93:0000" = "1E30:12BA"; # GeForce RTX 2080 SUPER Mobile / Max-Q
               "1E07:0000" = "1E30:12BA"; # GeForce RTX 2080 Ti Rev. A
             };
-            profileOverrides = {
-              # GRID RTX6000-1Q
-              "256" = {
-                vramAllocation = 1024;
-                heads = 4;
-                enableCuda = true;
-                display = {
-                  width = 7680;
-                  height = 4320;
-                };
-                framerateLimit = 0;
-              };
-              # GRID RTX6000-6Q
-              "260" = {
-                vramAllocation = 6144;
-                heads = 4;
-                enableCuda = true;
-                display = {
-                  width = 7680;
-                  height = 4320;
-                };
-                framerateLimit = 0;
-              };
-              # GRID RTX6000-24Q
-              "263" = {
-                vramAllocation = 16384;
-                heads = 4;
-                enableCuda = true;
-                display = {
-                  width = 7680;
-                  height = 4320;
-                };
-                framerateLimit = 0;
-              };
-            };
+            # profileOverrides = {
+            #   # GRID RTX6000-1Q
+            #   "256" = {
+            #     vramAllocation = 1024;
+            #     heads = 4;
+            #     enableCuda = true;
+            #     display = {
+            #       width = 7680;
+            #       height = 4320;
+            #     };
+            #     framerateLimit = 0;
+            #   };
+            #   # GRID RTX6000-6Q
+            #   "260" = {
+            #     vramAllocation = 6144;
+            #     heads = 4;
+            #     enableCuda = true;
+            #     display = {
+            #       width = 7680;
+            #       height = 4320;
+            #     };
+            #     framerateLimit = 0;
+            #   };
+            #   # GRID RTX6000-24Q
+            #   "263" = {
+            #     vramAllocation = 16384;
+            #     heads = 4;
+            #     enableCuda = true;
+            #     display = {
+            #       width = 7680;
+            #       height = 4320;
+            #     };
+            #     framerateLimit = 0;
+            #   };
+            # };
           };
           driverSource = {
             name  = "NVIDIA-Linux-x86_64-550.90.05-vgpu-kvm.run";
