f9a70a333f42608330ec015dd28efdfe3efb64f3
Yeshey <yesheysangpo@hotmail.com>
2025-08-20 22:59:59 +0200

    kubo just has to be compleatly nuked for now

--- nixos/skyloft/default.nix
+++ nixos/skyloft/default.nix
@@ -171,7 +171,7 @@
       listenAddress = "0.0.0.0";
     };
     luanti.enable = true;
-    kubo.enable = false;
+    kubo.enable = true;
     mindustry-server.enable = false;
     searx.enable = true;
     ollama = {
