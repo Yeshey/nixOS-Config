3da97832c5b5bc3eb0194c4ecd6f661d768c1ed6
Yeshey <yesheysangpo@hotmail.com>
2025-11-03 09:00:06 +0100

    hm

---
+++ modules/nixos/mySystem/autoUpdatesOnShutdown.nix
@@ -0,0 +1,233 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.mySystem.autoUpdatesOnShutdown;
+  notify-send-all = pkgs.writeShellScriptBin "notify-send-all" ''
+# https://github.com/tonywalker1/notify-send-all/tree/main
+# MIT License
+#
+# Copyright (c) 2022  Tony Walker
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+
+display_help() {
+    echo "Send a notification to all logged-in GUI users."
+    echo ""
+    echo "Usage: notify-send-all [options] <summary> [body]"
+    echo ""
+    echo "Options:"
+    echo "  -? | --help    This text."
+    echo ""
+    echo "All options from notify-send are supported, see below..."
+    echo
+    ${pkgs.libnotify}/bin/notify-send --help
+    exit 1
+}
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -h | --help)
+            display_help
+            exit 1
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+for SOME_USER in /run/user/*; do
+    SOME_USER=$(basename "$SOME_USER")
+    if [ "$SOME_USER" = 0 ]; then
+#        echo "* Skipping root user."
+        :
+    else
+        /run/wrappers/bin/sudo -u $(id -u -n "$SOME_USER") \
+            DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/"$SOME_USER"/bus ${pkgs.libnotify}/bin/notify-send "$@"
+    fi
+done
+
+exit 0
+  '';
+in
+{
+  options.mySystem.autoUpdatesOnShutdown = {
+    enable = lib.mkEnableOption "autoUpdatesOnShutdown";
+    location = lib.mkOption {
+      type = lib.types.str;
+      example = "github:Yeshey/nixOS-Config";
+      description = ''
+        The flake location (e.g., github:user/repo, /path/to/config)
+      '';
+    };
+    host = lib.mkOption {
+      type = lib.types.str;
+      example = "kakariko";
+      description = ''
+        Name of config to build
+      '';
+    };
+    dates = lib.mkOption {
+      type = lib.types.str;
+      example = "weekly";
+      default = "*-*-1/3"; # every 3 days
+      description = ''
+        How frequently to update (systemd calendar format)
+      '';
+    };
+  };
+
+  config = lib.mkIf (config.mySystem.enable && cfg.enable) {
+
+    environment.systemPackages = with pkgs; [
+      libnotify
+      notify-send-all
+    ];
+
+    # Use a timer to activate the service that will execute preStop on shutdown and not reboot
+    systemd.timers.my-nixos-upgrade = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        Persistent = true; # If missed, run on boot
+        OnCalendar = cfg.dates;
+        Unit = "my-nixos-upgrade.service";
+      };
+    };
+
+    systemd.services.my-nixos-upgrade = 
+    let
+      nixos-rebuild = "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
+      flake = "${cfg.location}#${cfg.host}";
+      operation = "boot"; # switch doesn't work, gets stuck in `setting up tmpfiles`
+      
+      upgradeScript = pkgs.writeShellScriptBin "nixos-upgrade-flake" ''
+        echo "Upgrading NixOS from ${flake}..."
+        ${nixos-rebuild} ${operation} --flake ${flake}
+        echo "Upgrade completed successfully"
+      '';
+    in {
+      description = "NixOS Upgrade on Shutdown";
+      restartIfChanged = false;
+      unitConfig.X-StopOnRemoval = false;
+
+      environment = config.nix.envVars // {
+        inherit (config.environment.sessionVariables) NIX_PATH;
+        HOME = "/root";
+      } // config.networking.proxy.envVars;
+
+      path = with pkgs; [
+        coreutils
+        gnutar
+        xz.bin
+        gzip
+        gitMinimal
+        config.nix.package.out
+      ];
+
+      script = ''
+        ${notify-send-all}/bin/notify-send-all -u critical "Will upgrade on shutdown..."
+      '';
+
+      preStop = ''
+        FLAG_FILE="/etc/nixos-reboot-upgrade.flag"
+
+        if ! systemctl list-jobs | egrep -q 'poweroff.target.*start'; then
+          echo "Not powering off (reboot or other), creating flag to upgrade after reboot."
+          touch $FLAG_FILE
+        else
+          echo "Powering off, upgrading now..."
+          ${upgradeScript}/bin/nixos-upgrade-flake
+        fi
+      '';
+
+      unitConfig = {
+        Conflicts = "reboot.target";
+      };
+
+      wants = [
+        "network-online.target"
+        "nss-lookup.target"
+        "nix-daemon.service"
+        "systemd-user-sessions.service"
+        "systemd-oomd.service"
+      ];
+
+      after = [
+        "network-online.target"
+        "nss-lookup.target"
+        "nix-daemon.service"
+        "systemd-user-sessions.service"
+        "plymouth-quit-wait.service"
+        "thermald.service"
+        "systemd-oomd.service"
+      ];
+
+      requires = [
+        "network-online.target"
+        "nss-lookup.target"
+        "nix-daemon.service"
+        "systemd-user-sessions.service"
+        "systemd-oomd.service"
+      ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+        TimeoutStopSec = "10h"; # 10 hours max
+      };
+    };
+
+    # Ensure poweroff.target doesn't kill the upgrade too early
+    systemd.targets."poweroff" = {
+      unitConfig = { 
+        "JobTimeoutSec" = 36000; # 10h
+      };
+    };
+
+    # If it rebooted instead of powering off, check for flag and upgrade now
+    systemd.services.nixos-reboot-upgrade-check = {
+      description = "Check for upgrade flag file on boot";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      
+      script = ''
+        FLAG_FILE="/etc/nixos-reboot-upgrade.flag"
+
+        if [ -f "$FLAG_FILE" ]; then
+          echo "$FLAG_FILE present, activating my-nixos-upgrade.service for upgrade on shutdown..."
+          systemctl start my-nixos-upgrade.service
+          echo "Removing flag $FLAG_FILE"
+          rm "$FLAG_FILE"
+        fi
+      '';
+
+      serviceConfig = {
+        Type = "oneshot";
+      };
+    };
+
+  };
+}
