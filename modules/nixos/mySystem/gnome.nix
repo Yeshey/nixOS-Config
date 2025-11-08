20fa7527f6a9afe9aff9ca4bfcdc7cf04035542c
Yeshey <yesheysangpo@hotmail.com>
2025-11-08 23:26:54 +0100

    gnome should be able to read the video formats

--- modules/nixos/mySystem/gnome.nix
+++ modules/nixos/mySystem/gnome.nix
@@ -30,6 +30,14 @@ in
       ];
     };
 
+    # for audio and video properties in nautilus interface https://github.com/NixOS/nixpkgs/issues/53631
+    environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+      pkgs.gst_all_1.gst-plugins-good
+      pkgs.gst_all_1.gst-plugins-bad
+      pkgs.gst_all_1.gst-plugins-ugly
+      pkgs.gst_all_1.gst-plugins-base
+    ];
+
     services = {
       xserver = {
         enable = true;
