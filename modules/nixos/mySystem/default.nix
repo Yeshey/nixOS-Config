b26de86b83ffbcff987795429ca57a78bfd56a2e
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-11-14 16:01:09 +0100

    test this ig

--- modules/nixos/mySystem/default.nix
+++ modules/nixos/mySystem/default.nix
@@ -414,7 +414,7 @@ in
       };
 
       # networking.useNetworkd = true;
-      networking.firewall.enable = false;
+      # networking.firewall.enable = false;
 
       nixpkgs.config = {
         # allowUnsupportedSystem = true;
