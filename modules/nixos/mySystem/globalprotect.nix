55949cf2504178a1369dab949ad96b37ac1deba5
Yeshey <yesheysangpo@hotmail.com>
2025-12-02 18:28:30 +0100

    again desktop file to connect to iscte

--- modules/nixos/mySystem/globalprotect.nix
+++ modules/nixos/mySystem/globalprotect.nix
@@ -16,9 +16,20 @@ in
 
   config = lib.mkIf (config.mySystem.enable && cfg.enable) {  
 
-    # environment.systemPackages = [ 
-    #   inputs.globalprotect-openconnect.packages.${pkgs.system}.default
-    # ];
+    environment.systemPackages = [ 
+      # inputs.globalprotect-openconnect.packages.${pkgs.system}.default
+      pkgs.gpclient
+
+      (pkgs.makeDesktopItem {
+        name = "gpclient-iscte";
+        desktopName = "GlobalProtect ISCTE-IUL";
+        comment = "Connect to ISCTE-IUL VPN";
+        exec = "sudo -E gpclient --fix-openssl connect --browser default vpn.iscte-iul.pt";
+        icon = "network-vpn";
+        categories = [ "Network" ];
+        terminal = true;  # This opens in your default terminal
+      })
+    ];
 
   };
 }
@@ -26,3 +37,5 @@ in
 # Launch GlobalProtect on App launcher
 # go into vpn.iscte-iul.pt
 # Fill your credentials
+# activate with: 
+# sudo -E gpclient --fix-openssl connect --browser default vpn.iscte-iul.pt
