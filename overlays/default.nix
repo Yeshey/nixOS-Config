ed7edbc59b8a4315063fc0737fe18c3592dee02f
Yeshey <yesheysangpo@hotmail.com>
2026-01-01 21:36:42 +0000

    I think this fixes the system warning

--- overlays/default.nix
+++ overlays/default.nix
@@ -24,7 +24,7 @@
   # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
+      localSystem = final.stdenv.hostPlatform;
       config = {
         allowUnfree = true;
         # permittedInsecurePackages = [
