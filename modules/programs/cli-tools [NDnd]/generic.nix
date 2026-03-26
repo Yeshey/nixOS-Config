bd493dce74bd56a07ba3facc3a4f7c35a256dd49
Yeshey <yesheysangpo@gmail.com>
2026-03-26 22:32:25 +0000

    list yo

--- modules/programs/cli-tools [NDnd]/generic.nix
+++ modules/programs/cli-tools [NDnd]/generic.nix
@@ -1,68 +1,54 @@
 let
-  genericPackages =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = with pkgs; [
-        git
-        tmux
-        htop
-        unstable.devenv
-        curl
-        vim # The Nano editor is installed by default.
-        wget
-        tree
-        btop
-        jq
-        xdg-utils
-        home-manager
-        local.cowsay
-      ];
-    };
+  genericPackages = pkgs: with pkgs; [
+    git
+    tmux
+    htop
+    unstable.devenv
+    curl
+    vim
+    wget
+    tree
+    btop
+    jq
+    xdg-utils
+    home-manager
+    local.cowsay
+  ];
 in
 {
   # Called by cli-tools
-  flake.modules.nixos.cli-tools = {
-    imports = [
-      genericPackages
-    ];
-  };
+  flake.modules.nixos.cli-tools = 
+    { pkgs, ... }: 
+    {
+      environment.systemPackages = genericPackages pkgs;
+    };
 
-  flake.modules.darwin.cli-tools = {
-    imports = [
-      genericPackages
-    ];
-  };
+  flake.modules.darwin.cli-tools =
+    { pkgs, ... }: 
+    {
+      environment.systemPackages = genericPackages pkgs;
+    };
 
   flake.modules.homeManager.cli-tools =
     { pkgs, ... }:
     {
-      home.packages = with pkgs; [
-        git
-        tmux
-        htop
-        unstable.devenv
-        curl
-        vim # The Nano editor is installed by default.
-        wget
-        tree
-        btop
-        jq
-        xdg-utils
-
-        ffmpeg-full
-        yt-dlp # download youtube videos
-        rsync
-        file
-        unzip
-        killall
-        cmatrix
-        scrcpy # screen cast android phone
-        ocrmypdf
-        libnotify # so you can use notify-send
-        xdotool
-        gh
-        android-tools
-        rclone
-      ];
+      home.packages =
+        (genericPackages pkgs)
+        ++ (with pkgs; [
+          ffmpeg-full
+          yt-dlp # download youtube videos
+          rsync
+          file
+          unzip
+          killall
+          cmatrix
+          scrcpy # screen cast android phone
+          ocrmypdf
+          libnotify # so you can use notify-send
+          xdotool
+          gh
+          android-tools
+          rclone
+        ]);
     };
 }
--- modules/system/settings/bluetooth [N]/bluetooth.nix
+++ modules/system/settings/bluetooth [N]/bluetooth.nix
@@ -1,6 +1,5 @@
 { ... }:
 {
-
   flake.modules.nixos.bluetooth = {
     hardware.bluetooth.enable = true;
     hardware.bluetooth.powerOnBoot = true;
