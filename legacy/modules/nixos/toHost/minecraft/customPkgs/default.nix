3c9cb51d587ccf4d2519bcebd3b9b53f5c992c63
Yeshey <yesheysangpo@gmail.com>
2026-03-31 07:40:13 +0100

    good riddence

--- legacy/modules/nixos/toHost/minecraft/customPkgs/default.nix
+++
@@ -1,6 +0,0 @@
-# this forge-servers config from https://github.com/Stefanuk12/nixos-config/blob/main/system/vps/minecraft/servers/fearNightfall/default.nix
-{ pkgs, ... }:
-
-{
-    forgeServers = pkgs.callPackage ./forge-servers/default.nix {};
-}
--- legacy/modules/nixos/toHost/remoteWorkstation/default.nix
+++
@@ -1,6 +0,0 @@
-{
-   imports = [
-    ./xrdp.nix
-    ./sunshine.nix
-   ];
-}
