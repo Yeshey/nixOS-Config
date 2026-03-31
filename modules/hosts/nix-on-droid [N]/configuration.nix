a10d15b9375fc54989b5dc9419bd1f6f66c9d95f
Yeshey <yesheysangpo@gmail.com>
2026-03-31 23:53:30 +0100

    nix-on-droid

--- modules/hosts/nix-on-droid [N]/configuration.nix
+++ modules/hosts/nix-on-droid [N]/configuration.nix
@@ -12,20 +12,22 @@
       android-integration.xdg-open.enable = true;
 
       environment.packages = with pkgs; [
-        curl vim neovim tmux wget tree btop git
-        procps killall diffutils findutils
-        tzdata hostname man
-        gnugrep gnupg gnused gnutar
-        bzip2 gzip xz zip unzip
-        nix zsh
+        tmux wget tree git
       ];
 
       home-manager.config =
         { ... }:
         {
-          imports = [
-            inputs.self.modules.homeManager.shell
-            inputs.self.modules.homeManager.starship
+          imports = with inputs.self.modules.homeManager; [
+            shell
+            starship
+            direnv
+            cli-tools
+            nh
+            nix-index-database
+            nix-your-shell
+            gc
+            safe-rm
           ];
 
           programs.git = {
@@ -41,7 +43,6 @@
               clean  = lib.mkForce "nix-collect-garbage -d && nix-store --gc && echo 'Displaying stray roots:' && nix-store --gc --print-roots | egrep -v '^(/nix/var|/run/current-system|/run/booted-system|/proc|\\{memory|\\{censored)'";
             };
             initContent = lib.mkBefore ''
-              autoload -U compinit && compinit
               sshd-start
             '';
           };
