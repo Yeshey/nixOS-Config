c7534069f34d5cff89c88854a9bf8edfa1cae247
Yeshey <yesheysangpo@gmail.com>
2026-04-02 23:18:23 +0100

    typo

--- modules/programs/zed-editor [n]/zed-editor.nix
+++ modules/programs/zed-editor [n]/zed-editor.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 {
-  flake.modules.homeManager.zed-editor-host =
+  flake.modules.homeManager.zed-editor =
     {
       imports = with inputs.self.modules.homeManager; [
         zed-editor-client
