ef9948f5467b0ded6f57a7d1d50cd6d69035af89
Yeshey <yesheysangpo@gmail.com>
2026-04-13 23:19:31 +0100

    helper function for comma to also search for unfree packages

--- modules/nix/tools/nix-index-database [Nn]/nix-index-database.nix
+++ modules/nix/tools/nix-index-database [Nn]/nix-index-database.nix
@@ -1,11 +1,48 @@
 { inputs, ... }:
+let
+  commaUnfreeFunction = ''
+    function , {
+      command , "$@" && return
+
+      local cmd="$1"
+      echo "Trying unfree fallback for '$cmd'..."
+
+      local attr
+      attr=$(NIXPKGS_ALLOW_UNFREE=1 nix search nixpkgs "^$cmd$" --json 2>/dev/null \
+        | jq -r --arg cmd "$cmd" 'keys | map(select(test("\\." + $cmd + "$"))) | .[0] // empty' \
+        | sed 's/legacyPackages\.[^.]*\.//')
+
+      if [[ -z "$attr" ]]; then
+        attr=$(NIXPKGS_ALLOW_UNFREE=1 nix search nixpkgs "$cmd" --json 2>/dev/null \
+          | jq -r 'keys[0] // empty' \
+          | sed 's/legacyPackages\.[^.]*\.//')
+      fi
+
+      if [[ -n "$attr" ]]; then
+        echo "Found nixpkgs#$attr, launching with unfree enabled..."
+        NIXPKGS_ALLOW_UNFREE=1 nix shell --impure "nixpkgs#$attr" -c "$@"
+      else
+        echo "No package found for '$cmd' even with unfree enabled."
+        return 1
+      fi
+    }
+  '';
+in
 {
   flake.modules.nixos.nix-index-database = {
     imports = [ inputs.nix-index-database.nixosModules.nix-index ];
     programs.nix-index-database.comma.enable = true;
   };
-  flake.modules.homeManager.nix-index-database = {
-    imports = [ inputs.nix-index-database.homeModules.nix-index ];
-    programs.nix-index-database.comma.enable = true;
-  };
+
+  flake.modules.homeManager.nix-index-database =
+    { pkgs, lib, ... }:
+    {
+      imports = [ inputs.nix-index-database.homeModules.nix-index ];
+      programs.nix-index-database.comma.enable = true;
+
+      home.packages = [ pkgs.jq ];
+
+      programs.zsh.initContent = lib.mkBefore commaUnfreeFunction;
+      programs.bash.initExtra = lib.mkBefore commaUnfreeFunction;
+    };
 }
