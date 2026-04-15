bbdef8894cacc1e56195c7d068e6b39913127e9c
Yeshey <yesheysangpo@gmail.com>
2026-04-15 03:39:25 +0100

    the useer defines the name of the machine to build in host

--- modules/system/settings/upgrade-on-shutdown [N]/upgrade-on-shutdown.nix
+++ modules/system/settings/upgrade-on-shutdown [N]/upgrade-on-shutdown.nix
@@ -1,12 +1,13 @@
 {
   flake.modules.nixos.upgrade-on-shutdown =
-    { lib, ... }:
+    { lib, config, ... }:
     {
       system.autoUpgrade.enable = lib.mkForce false;
 
       system.autoUpgradeOnShutdown = {
         enable = true;
-        flake  = "github:yeshey/nixos-config";
+        flake = "github:yeshey/nixos-config";
+        host = config.networking.hostName;
         dates  = "*-*-01,16 06:10:00";
         extraKeepAliveServices = [ "fix-surface-clock.service" "autossh-reverseProxy.service" ];
       };
