09f09332b3e50de8dc115ba9894b8ad5ba4d9485
yeshey <yesheysangpo@hotmail.com>
2026-04-22 04:24:24 +0200

    so, steamcmd works, but I want to know if I can simply override steam-run in steam and steamcmd isntead of doing things manually

--- modules/system/settings/box64-binfmt [N]/box64-binfmt.nix
+++ modules/system/settings/box64-binfmt [N]/box64-binfmt.nix
@@ -3,28 +3,22 @@
   flake.modules.nixos.box64-binfmt =
     { pkgs, ... }:
     {
-      nixpkgs.overlays = [
-        inputs.box64-binfmt.overlays.default 
-      ];
+      # nixpkgs.overlays = [
+      #   inputs.box64-binfmt.overlays.default 
+      # ];
 
-      # Required so the build system can cross-compile the box32 package before
-      # binfmt registration is active.  Rebuild once with enable = false first.
-      boot.binfmt.emulatedSystems = [
-        "i386-linux" "i486-linux" "i586-linux" "i686-linux"
-        "x86_64-linux"
-      ];
-      nix.settings.extra-platforms = [
-        "i386-linux" "i486-linux" "i586-linux" "i686-linux"
-        "x86_64-linux"
-      ];
+      # # Required so the build system can cross-compile the box32 package before
+      # # binfmt registration is active.  Rebuild once with enable = false first.
+      # boot.binfmt.emulatedSystems = [
+      #   "i386-linux" "i486-linux" "i586-linux" "i686-linux"
+      #   "x86_64-linux"
+      # ];
+      # nix.settings.extra-platforms = [
+      #   "i386-linux" "i486-linux" "i586-linux" "i686-linux"
+      #   "x86_64-linux"
+      # ];
 
       box64-binfmt.enable = true;
-
-      nixpkgs.config.permittedInsecurePackages = [
-        "python3.12-ecdsa-0.19.1"
-      ];
-
-      security.unprivilegedUsernsClone = true;
 
       environment.systemPackages = [
         pkgs.x86.cowsay
