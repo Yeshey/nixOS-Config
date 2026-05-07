746837b0e86d1fb0210a9ca45f29b18d3b6f36b3
Yeshey <yesheysangpo@gmail.com>
2026-05-07 01:31:06 +0100

    every 2 days instead of every day

--- modules/hosts/hyrulecastle [N]/users/yeshey.nix
+++ modules/hosts/hyrulecastle [N]/users/yeshey.nix
@@ -22,8 +22,8 @@ in
           rcloneRemotePath   = "Backups/ResticBackups/mainBackupOneDrive";
           passwordFile       = builtins.toFile "restic-password" "123456789";
           initialize         = false;
-          startAt            = "*-*-* 14:00:00";
-          randomizedDelaySec = "6h";
+          startAt            = "*-*-1/2 14:00:00";
+          randomizedDelaySec = "1h";
           prune.keep         = { within = "1d"; daily = 2; weekly = 2; monthly = 6; yearly = 3; };
           exclude            = [ "**/.var" "**/RecordedClasses" "**/Games" ];
         };
