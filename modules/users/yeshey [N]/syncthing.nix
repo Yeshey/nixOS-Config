f06474e529eeab9a7b515801bc884915790cf67d
Yeshey <yesheysangpo@gmail.com>
2026-05-20 21:07:59 +0100

    lunati mintestes syncthing

--- modules/users/yeshey [N]/syncthing.nix
+++ modules/users/yeshey [N]/syncthing.nix
@@ -178,7 +178,7 @@ in
           !/*.cfg
           *
         '';
-        ".minetest/.stignore".text = ''
+        ".var/app/org.luanti.luanti/.minetest/.stignore".text = ''
           !/games
           !/worlds
           *
