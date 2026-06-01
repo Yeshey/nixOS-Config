a772e1ad5b05d1d24ace3a209182f5da0c2eee2d
Yeshey <yesheysangpo@gmail.com>
2026-06-02 00:11:04 +0100

    syncthing for now my computer is the source of truth

--- modules/hosts/hyrulecastle [N]/syncthing.nix
+++ modules/hosts/hyrulecastle [N]/syncthing.nix
@@ -4,13 +4,14 @@
       settings = {
         folders = {
           "PersonalFiles" = {
-          ignorePatterns = lib.mkForce (
-            [
-              "/Timeless/Syncthing/WhatsAppMovies"
-              "/Timeless/Syncthing/WhatsAppPictures"
-              "/Timeless/Syncthing/A70Camera"
-            ] ++ (import (builtins.toPath "${./../..}/users/yeshey [Nn]/syncthing [N]/global-excludes.nix-data"))
-          );
+            type = "sendonly";
+            ignorePatterns = lib.mkForce (
+              [
+                "/Timeless/Syncthing/WhatsAppMovies"
+                "/Timeless/Syncthing/WhatsAppPictures"
+                "/Timeless/Syncthing/A70Camera"
+              ] ++ (import (builtins.toPath "${./../..}/users/yeshey [Nn]/syncthing [N]/global-excludes.nix-data"))
+            );
           };
         };
       };
