41b636489078e779abe09152be664895cad0e840
Yeshey <yesheysangpo@gmail.com>
2026-06-04 21:44:14 +0100

    syncthing is a bitch

--- modules/hosts/hyrulecastle [N]/syncthing.nix
+++ modules/hosts/hyrulecastle [N]/syncthing.nix
@@ -1,21 +1,21 @@
 {
   flake.modules.nixos.hyrulecastle = { lib, ... }: {
-    services.syncthing = {
-      settings = {
-        folders = {
-          "PersonalFiles" = {
-            type = "sendonly";
-            # I don't want to sync everything anymore
-            # ignorePatterns = lib.mkForce (
-            #   [
-            #     "/Timeless/Syncthing/WhatsAppMovies"
-            #     "/Timeless/Syncthing/WhatsAppPictures"
-            #     "/Timeless/Syncthing/A70Camera"
-            #   ] ++ (import (builtins.toPath "${./../..}/users/yeshey [Nn]/syncthing [N]/global-excludes.nix-data"))
-            # );
-          };
-        };
-      };
-    };
+    # services.syncthing = {
+    #   settings = {
+    #     folders = {
+    #       "PersonalFiles" = {
+    #         type = "sendonly";
+    #         # I don't want to sync everything anymore
+    #         ignorePatterns = lib.mkForce (
+    #           [
+    #             "/Timeless/Syncthing/WhatsAppMovies"
+    #             "/Timeless/Syncthing/WhatsAppPictures"
+    #             "/Timeless/Syncthing/A70Camera"
+    #           ] ++ (import (builtins.toPath "${./../..}/users/yeshey [Nn]/syncthing [N]/global-excludes.nix-data"))
+    #         );
+    #       };
+    #     };
+    #   };
+    # };
   };
 }
