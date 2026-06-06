9a3c09eb73e781dc6828ab84b3a35375958dfa3a
Yeshey <yesheysangpo@gmail.com>
2026-06-06 14:03:17 +0100

    added bing wallpaper extension to hyrulecastle host

--- modules/hosts/hyrulecastle [N]/gnome-customisation.nix
+++ modules/hosts/hyrulecastle [N]/gnome-customisation.nix
@@ -1,9 +1,38 @@
+let 
+  username = "yeshey"; 
+in
 {
   flake.modules.nixos.hyrulecastle = 
     { lib, config, ... }: 
     {
       config = lib.mkIf config.systemConstants.isGnome {
         services.displayManager.gdm.autoSuspend = false;
+      };
+    };
+
+  flake.modules.homeManager.${username} =
+    { pkgs, lib, osConfig, ... }:
+    {
+      config = lib.mkIf (osConfig.systemConstants.isGnome or false) {
+        home.packages = with pkgs; [
+          gnomeExtensions.bing-wallpaper-changer
+        ];
+
+        dconf.settings = {
+          "org/gnome/shell" = {
+            disable-user-extensions = false;
+            disabled-extensions = [
+            ];
+            enabled-extensions = [
+              "BingWallpaper@ineffable-gmail.com"
+            ];
+          };
+
+          "org/gnome/shell/extensions/bingwallpaper" = {
+            hide = true;
+            notify = true;
+          };
+        };
       };
     };
 }
