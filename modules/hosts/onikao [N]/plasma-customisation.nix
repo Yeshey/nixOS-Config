87df378afe26a288c40a91b22e74e0a5d98360d6
Yeshey <yesheysangpo@gmail.com>
2026-09-01 19:58:36 +0100

    the fuck fuck you fuck

---
+++ modules/hosts/onikao [N]/plasma-customisation.nix
@@ -0,0 +1,43 @@
+{
+  inputs,
+  ...
+}:
+{
+  flake.modules.homeManager.onikao =
+    { inputs, pkgs, lib, osConfig, ... }:
+
+    let
+      # A wrapper script to run xautolock with the desired options
+      # This avoids quoting issues in the .desktop file
+      xautolockWrapper = pkgs.writeShellScript "xautolock-wrapper" ''
+        # 1 minute idle timeout (change to 10 later)
+        ${pkgs.xautolock}/bin/xautolock -time 1 \
+          -locker "${pkgs.libnotify}/bin/notify-send 'Idle' 'Triggered'" \
+          -detectsleep
+      '';
+    in {
+      imports = [
+        inputs.plasma-manager.homeModules.plasma-manager
+      ];
+
+      #config = lib.mkIf (osConfig.systemConstants.isKdePlasma or false) {
+      config = {
+        # 1. Ensure xautolock is installed
+        home.packages = [ pkgs.xautolock pkgs.cowsay ];
+
+        # 2. Create an autostart .desktop file
+        xdg.configFile."autostart/xautolock.desktop".text = ''
+          [Desktop Entry]
+          Type=Application
+          Name=Idle Logout (xautolock)
+          Exec=${xautolockWrapper}
+          X-KDE-autostart-condition=ksmserver
+          X-GNOME-Autostart-enabled=true
+          StartupNotify=false
+        '';
+
+        # 3. Remove the old PowerDevil config – it's not needed
+        # programs.plasma.configFile."powerdevilrc"."AC/RunScript" ... (delete this)
+      };
+    };
+}
