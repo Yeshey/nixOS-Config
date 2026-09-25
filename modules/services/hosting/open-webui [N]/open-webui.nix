0d9333ccddd70361d56671e2b0898ec7dee023c2
Yeshey <yesheysangpo@gmail.com>
2026-09-25 20:11:21 +0100

    add cerebras

--- modules/services/hosting/open-webui [N]/open-webui.nix
+++ modules/services/hosting/open-webui [N]/open-webui.nix
@@ -20,10 +20,11 @@
       sops.secrets."opencode_key" = { };
       sops.secrets."groq_key" = { };
       sops.secrets."vercel_key" = { };
+      sops.secrets."cerebras_key" = { };
 
       sops.templates."open-webui.env" = {
         content = ''
-          OPENAI_API_KEYS=${config.sops.placeholder."litellm_master_key"};${config.sops.placeholder."nvidia_nim_api_key"};${config.sops.placeholder."openrouter"};${config.sops.placeholder."opencode_key"};${config.sops.placeholder."groq_key"};${config.sops.placeholder."vercel_key"}
+          OPENAI_API_KEYS=${config.sops.placeholder."litellm_master_key"};${config.sops.placeholder."nvidia_nim_api_key"};${config.sops.placeholder."openrouter"};${config.sops.placeholder."opencode_key"};${config.sops.placeholder."groq_key"};${config.sops.placeholder."vercel_key"};${config.sops.placeholder."cerebras_key"}
         '';
         restartUnits = [ "open-webui.service" ];
       };
@@ -65,8 +66,8 @@
           ENABLE_TITLE_GENERATION = "false";
           OLLAMA_API_BASE_URL = "http://localhost:11434";
 
-          OPENAI_API_BASE_URLS = "http://localhost:${toString litellmPort}/v1;https://integrate.api.nvidia.com/v1;https://openrouter.ai/api/v1;https://opencode.ai/zen/v1;https://api.groq.com/openai/v1;https://ai-gateway.vercel.sh/v1";
-          OPENAI_API_CONFIGS = ''{"0": {"prefix_id": "googleAPI/"}, "1": {"prefix_id": "nvidiaAPI/"}, "2": {"prefix_id": "openrouterAPI/"}, "3": {"prefix_id": "opencodeAPI/"}, "4": {"prefix_id": "groqAPI/"}, "5": {"prefix_id": "vercelAPI/"}}'';
+          OPENAI_API_BASE_URLS = "http://localhost:${toString litellmPort}/v1;https://integrate.api.nvidia.com/v1;https://openrouter.ai/api/v1;https://opencode.ai/zen/v1;https://api.groq.com/openai/v1;https://ai-gateway.vercel.sh/v1;https://api.cerebras.ai/v1";
+          OPENAI_API_CONFIGS = ''{"0": {"prefix_id": "googleAPI/"}, "1": {"prefix_id": "nvidiaAPI/"}, "2": {"prefix_id": "openrouterAPI/"}, "3": {"prefix_id": "opencodeAPI/"}, "4": {"prefix_id": "groqAPI/"}, "5": {"prefix_id": "vercelAPI/"}, "6": {"prefix_id": "cerebrasAPI/"}}'';
 
           WEBUI_AUTH = "False";
         };
